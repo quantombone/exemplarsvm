85d4f2dd46f2675482dc297d59de09a764125a54
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-03-21 02:57:11 -0400

    changed them

--- nips11/demo_capacity.m
+++ nips11/demo_capacity.m
@@ -1,12 +1,13 @@
 function demo_capacity
 %% try a nonlinear decision boundary
 
-SVMC = .1;
+SVMC = 1;
 %xs = linspace(0,4*pi,1000);
 %xs2 = sin(xs);
+rand('seed',1234);
 t = linspace(0,2*pi,300);
 xpos = [10*cos(t); 10*sin(t)]
-xneg = [3*cos(t); 3*sin(t)];
+xneg = [4*cos(t); 4*sin(t)];
 
 xpos = xpos + randn(size(xpos));
 xneg = xneg + randn(size(xneg));
@@ -28,12 +29,14 @@ neg = find(y==-1);
 %clf
 %subplot(1,2,1)
 
+curscores = [];
+
 y = y';
 
 gammas = linspace(.00001,100,size(x,2));
 for iii = 1:1:size(xpos,2)
-  index = iii
-  gamma = 1;
+  index = iii;
+  gamma = 1.0;
   %gamma = gammas(iii); %.001;
   %gammaK = iii;
   %gammaK = 0;
@@ -51,8 +54,8 @@ plot(x(1,index),x(2,index),'kp','MarkerSize',12,'LineWidth',3)
 
 ds = distSqr_fast(xpos(:,index),xpos);
 [aa,bb] = sort(ds);
-curx = [xpos(:,bb(1:50)) xneg];
-cury = [ypos(bb(1:50)) yneg]';
+curx = [xpos(:,bb(1:1)) xneg];
+cury = [ypos(bb(1:1)) yneg]';
 
 [w,b] = learn_local_no_capacity(curx,cury,index,SVMC);
 res = w'*x-b;
@@ -98,6 +101,8 @@ plot(x(1,index),x(2,index),'kp','MarkerSize',12,'LineWidth',3)
 
 [w,b,alphas,pos_inds] = learn_local_capacity(x,y,index,SVMC,gamma);
 
+curscores(end+1,:) = w'*x-b;
+
 res = w'*x-b;
 
 xmin = bbox_range(1);
@@ -126,94 +131,19 @@ hold on;
 plot([xmin xmax],[yminpos ymaxpos],'r--','LineWidth',2)
 hold on;
 
-
 goods = pos_inds(alphas>0);
-
 plot(x(1,goods),x(2,goods),'ko');
 title(sprintf('Max-Capacity \\gamma=%.3f',gamma))
+axis(bbox_range);
 
-axis(bbox_range)
-
+index
 drawnow
-
 end
 
+figure(2)
+imagesc(curscores)
+keyboard
 
-function [w,b,alphas,pos_inds] = learn_local_capacity(x,y,index,SVMC,gamma)
-%maximum-capacity learning
-pos_inds = find(y==y(index));
-
-flip = 0;
-if y(index) == -1
-  flip = 1;
-  y = y *-1;
-end
-
-g = ones(size(pos_inds));
-
-%turn all negatives on, they always stay on
-%turn all positives off at start
-alphas = y*0+1;
-alphas(pos_inds)=0;
-
-%turn self on (its a positive)
-alphas(index) = 1;
-
-oldgoods = [];
-for k = 1:20
-  goods = find(alphas==1);
-  if length(oldgoods) > 0
-    diffinds = setdiff(goods,oldgoods);
-    if length(diffinds) == 0
-      alphas = alphas(pos_inds);
-      return;
-    end
-  end
-
-  oldgoods = goods;
-  frac=(sum(alphas)-sum(y==-1)) / length(pos_inds);
-  
-  svm_model = svmtrain(y(goods), x(:,goods)',sprintf(['-s 0 -t 0 -c' ...
-                    ' %f -q'],SVMC));
-  
-  
-  svm_weights = full(sum(svm_model.SVs .* ...
-                         repmat(svm_model.sv_coef,1,size(svm_model.SVs,2)),1));
-  w = svm_weights';
-  b = svm_model.rho;
-  
-  if 1
-    %%optimize alphas 
-    if 1
-      %gamma term
-      hinge = @(x)max(1-x,0.0);
-      loss_term = hinge((w'*x(:,pos_inds)) .* y(pos_inds)');
-      alphas(pos_inds) = loss_term < gamma*g';  
-    % else
-    %   %%TOPK
-    %   loss_term = (1-(w'*x(:,pos_inds)) .* y(pos_inds)');
-    %   [alpha,beta] = sort(loss_term);
-    %   alphas(pos_inds) = 1;
-    %   alphas(pos_inds(beta(K+1:end)))=0;
-    end
-    
-    %fprintf(1,'hack non-enable sellf\n');
-    %turn self on
-    %alphas(index) = 1;
-  end
-  
-  %fprintf(1,'frac +: %.3f, raw=%d\n',frac,sum(alphas)-sum(y==-1));
-  
-end
-
-fprintf(1,' --frac +: %.3f, raw=%d\n',frac,sum(alphas)-sum(y==-1));
-
-if flip == 1
-  w = w*-1;
-  b = b*-1;
-end
-
-alphas = alphas(pos_inds);
 
 function [w,b] = learn_local_no_capacity(x,y,index,SVMC)
 
--- nips11/train_regressor.m
+++ nips11/train_regressor.m
@@ -1,207 +1,289 @@
 function m = train_regressor(m)
+%Train a linear function which regress well onto other objects and
+%has max-margin separation between negatives
 VOCinit;
 m.models_name = 'nips11';
 
-fg = get_pascal_bg('trainval',m.cls);
+fg = get_pascal_bg('train',m.cls);
 
-fg = fg(1:40);
 [a,b,c] = fileparts(fg{end});
-fg(2:end+1) = fg;
-fg{1} = [a '/'  m.curid c];
 
-bg = get_pascal_bg('trainval',['-' m.cls]);
-bg = bg(1:5);
+starter = [a '/'  m.curid c];
+fg = setdiff(fg,starter);
+fg = [starter; fg];
 
-fg = [fg; bg];
+%(bg) is already inside of m
 
+%[os,xcat,X,ids,imageid] = get_dets(m,fg);  
+load myfiles.mat  
+os2 = os;
+xcat2 = xcat;
+X2 = X;
+ids2 = ids;
+imageid2 = imageid;
+
+  
+
+for qqq = 1:50
+  m = learn_model(m,os,xcat,X,ids);
+  
+  [os,xcat,X,ids,imageid] = collect_top_dets(m,os2,xcat2,X2,ids2, ...
+                                           imageid2);
+
+  %os(end+1:end+size(m.model.x,2))=1;
+  
+  %X(end+1,:) = 1;
+  
+  % [aa,bb] = sort(os+exp(m.model.w(:)'*X(1:end-1,:)-m.model.b)', ...
+  %                'descend');
+  %os = os+exp(m.model.w(:)'*X(1:end-1,:)-m.model.b)';
+  
+  %[aa,bb] = sort(m.model.w(:)'*X(1:end-1,:),'descend');
+  %X = X(:,bb(1:length(bb)/10));
+  %os = os(bb(1:length(bb)/10));
+  
+  % [aa,bb] = sort(m.model.w(:)'*X(1:end-1,:),'descend');
+  % [alpha,beta] = sort(bb);
+  % ranks = (beta);
+  
+  % rankscores = ranks.^-.1;
+  % bads = find(os<.2);
+  % os = os .* rankscores';
+  % os = reshape(os,1,[]);
+  % os(bads) = 0;
+  
+  % %w = (os*inv(X'*X+lambda*eye(size(X,2),size(X,2)))*X')';
+  % %w2 = inv(X*X'+lambda*eye(size(X,1),size(X,1)))*X*os';
+  
+  % %alpha = pinv(X)*w;
+  % %alpha2 = pinv(X)*w2;
+  
+  % K = X'*X;
+  
+  % sigmoid = @(x)1./(1+exp(-x));
+  % lambda = 1;
+  % %a = fminunc(@(a)norm(sigmoid(K*a)-os').^2+lambda*norm(X*a)^2,...
+  % %            zeros(size(X,2),1),...
+  % %            optimset('MaxIter',100));
+  
+  % %This is the real dual problem
+  % a = inv(K*K+lambda*K)*K*os';
+  
+  % %get primal detector
+  % w = X*a;
+  
+  % %w = fminunc(@(w)objective(w,X,os),w,optimset('MaxIter',10));
+  
+  % m.model.b = w(end);
+  % m.model.w = reshape(w(1:end-1),m.model.hg_size);
+  
+  % figure(2)
+  % clf
+  % imagesc(HOGpicture(reshape(m.model.w,m.model.hg_size)))
+  % drawnow
+    
+  res = m.model.w(:)'*X-m.model.b;
+  
+  [aa,bb] = sort(res,'descend');
+  
+  figure(34)
+  clf
+  N = 4;
+  for q = 1:N*N
+    if q <= length(bb)
+      subplot(N,N,q)
+      I = convert_to_I(fg{imageid(bb(q))});
+      %imagesc(I)
+      curbb = ids{bb(q)}.bb;
+      
+      Ipad = pad_image(I,100);
+      curbb = round(curbb+100);
+      curI = Ipad(curbb(2):curbb(4),curbb(1):curbb(3),:);
+      %imagesc(I)
+
+      if ids{bb(q)}.flip == 1
+        curI = flip_image(curI);
+      end
+      imagesc(curI)
+      %plot_bbox(curbb)
+      title(sprintf('s=%.3f os=%.3f',aa(q),os(bb(q))))
+      axis off
+      axis image
+    end
+  end
+  
+  % figure(444)
+  % plot(res,os,'r.')
+  drawnow
+
+
+  %save m.mat m
+end
+
+% function [fval,gval] = objective(w,X,os)
+% lambda = 100;
+% keyboard
+% fval = norm(sigmoid(w'*X)-os).^2+lambda*norm(w);
+% gval = 2*(sigmoid(w'*X)-os)'*sigmoidp(w'*X)*X + 2*lambda*w';
+
+% function y = sigmoid(x)
+% y = 1./(1+exp(-x));
+
+% function y = sigmoidp(x)
+% y=exp(x)./(1+exp(x)).^2;
+
+function [os,xcat,X,ids,imageid] = get_dets(m,fg)
+VOCinit;
 localizeparams.thresh = -1;
-localizeparams.TOPK = 50;
+localizeparams.TOPK = 10;
 localizeparams.lpo = 10;
 localizeparams.SAVE_SVS = 1;
-   
-%figure(1)
-%clf
 
 c = 1;
-for qqq = 1:20
-
-clear results
-for i = 1:5%length(fg)
-  if c > length(fg)
-    return;
+  for i = 1:length(fg)
+    if c > length(fg)
+      return;
+    end
+    
+    I = convert_to_I(fg{c});
+    
+    [a,curid,other] = fileparts(fg{c});
+    
+    recs = PASreadrecord(sprintf(VOCopts.annopath,curid));  
+    [rs] = localizemeHOG(I,{m},localizeparams);
+    
+    %extract detection box vectors from the localization results
+    [coarse_boxes] = extract_bbs_from_rs(rs, {m});
+    
+    boxes = coarse_boxes;
+    %map GT boxes from training images onto test image
+    boxes = adjust_boxes(coarse_boxes,{m});
+    
+    
+    % figure(1)
+    % % clf
+    % subplot(3,3,i)
+    % imagesc(I)
+    % plot_bbox(boxes)
+    % drawnow
+    % axis image
+    % axis off
+    
+    %goods = find(ismember({recs.objects.class},m.cls));
+    gt_classes = {recs.objects.class};
+    goods = 1:length(gt_classes);
+    goodbbs = cat(1,recs.objects(goods).bbox);
+    %goodbbs(end+1,:) = [1 1 1 1];
+    %gt_classes(end+1) = '';
+    
+    osmat = getosmatrix_bb(boxes,goodbbs);
+    
+    [maxos,maxind] = max(osmat,[],2);
+    maxcat = gt_classes(maxind)';
+    if length(rs.support_grid{1})>0
+      maxval = max(m.model.w(:)'*cat(2,rs.support_grid{1}{:})- ...
+                   m.model.b);
+    else
+      maxval = -1;
+    end
+    fprintf(1,'maxos %d = %.3f, maxval=%.3f\n',c,max(maxos),maxval);
+    results{c}.maxos = maxos;
+    results{c}.maxcat = maxcat;
+    results{c}.id = maxos*0+c;
+    results{c}.id_grid = [rs.id_grid{1}];
+    results{c}.x = [];
+    if length(rs.support_grid{1})>0
+      results{c}.x = cat(2,rs.support_grid{1}{:});  
+    end
+    
+    c = c + 1;
   end
-  I = convert_to_I(fg{c});
-  [a,curid,other] = fileparts(fg{c});
-  c = c + 1;
-  recs = PASreadrecord(sprintf(VOCopts.annopath,curid));  
-  [rs] = localizemeHOG(I,{m},localizeparams);
 
   
-  %extract detection box vectors from the localization results
-  [coarse_boxes] = extract_bbs_from_rs(rs, {m});
+  os = cellfun2(@(x)x.maxos,results);
+  os = cat(1,os{:});
   
-  boxes = coarse_boxes;
-  %map GT boxes from training images onto test image
-  boxes = adjust_boxes(coarse_boxes,{m});
+  xcat = cellfun2(@(x)reshape(x.maxcat,[],1),results);
+  xcat = cat(1,xcat{:});
   
+  x = cellfun2(@(x)x.x,results);
+  x = cat(2,x{:});
+  X = x;
   
-   % figure(1)
-   % % clf
-   % subplot(3,3,i)
-   % imagesc(I)
-   % plot_bbox(boxes)
-   % drawnow
-   % axis image
-   % axis off
+  ids = cellfun2(@(x)x.id_grid,results);
+  ids = cat(2,ids{:});
   
-  goods = find(ismember({recs.objects.class},m.cls));
-  %goods = 1:length({recs.objects.class});
-  goodbbs = cat(1,recs.objects(goods).bbox);
-  
-  osmat = getosmatrix_bb(boxes,goodbbs);
-  
-  maxos = max(osmat,[],2);
-  fprintf(1,'maxos %d = %.3f\n',i,max(maxos));
-  results{i}.maxos = maxos;
-  results{i}.id = maxos*0+i;
-  results{i}.id_grid = [rs.id_grid{1}];
-  results{i}.x = [];
-  if length(rs.support_grid{1})>0
-    results{i}.x = cat(2,rs.support_grid{1}{:});  
-  end
+  imageid = cellfun2(@(x)x.id(:),results);
+  imageid = cat(1,imageid{:});
+
+function [os,xcat,X,ids,imageid] = collect_top_dets(m,os,xcat,X,ids, ...
+                                                  imageid)
+res = m.model.w(:)'*X-m.model.b;
+%% get top det per image
+uq = unique(imageid);
+keepers = zeros(size(X,2));
+
+for i = 1:length(uq)
+  cur = find(imageid==uq(i));
+  [aa,bb] = max(res(cur));
+  keepers(cur(bb))=1;
 end
+inds = find(keepers);
+os = os(inds);
+X = X(:,inds);
+xcat = xcat(inds);
+ids = ids(inds);
+imageid = imageid(inds);
 
-os = cellfun2(@(x)x.maxos,results)
-os = cat(1,os{:});
-x = cellfun2(@(x)x.x,results);
-x = cat(2,x{:});
-X = x;
+function m = learn_model(m,os,xcat,X,ids)
+tau = 0;
+gamma = .3;
+SVMC = .01;
 
-ids = cellfun2(@(x)x.id_grid,results);
-ids = cat(2,ids{:});
+y = double(os>tau & ismember(xcat,m.cls));
+y(y==0) = -1;
+g = os(y>0);
+g = 1./(1+exp(-10*(g-.5)));
 
-imageid = cellfun2(@(x)x.id(:),results);
-imageid = cat(1,imageid{:});
+%oldscores = m.model.w(:)'*X(:,y>0)-m.model.b;
+%g = g.* 1./(1+exp(-1*oldscores))';
 
-%X(end+1,:) = 0;
-%goods = find(os>.2);
-%X = X(:,goods);
-%os = os(goods);
+%[tmp,index] = max(results{1}.maxos);
+%size(X)
+%size(m.model.nsv)
 
-if 0
-  %playing with liblinear here
-  SVMC = 100;
-  
-  y = double(os>.5);
-  y(y==0) = -1;
-  model = liblinear_train(y, sparse(X)', sprintf(['-s 0 -B 1 -c' ...
-                    ' %f'],SVMC));
-  wex = model.w(1:end-1)';
-  b = -model.w(end);
-  if y(1) == -1
-    wex = wex*-1;
-    b = b*-1;
-  end
+X = cat(2,X,m.model.nsv);
+y = cat(1,y,-1*ones(size(m.model.nsv,2),1));
 
-  m.model.b = b;
-  m.model.w = reshape(wex,m.model.hg_size);
-end
+index = 1;
 
-X = [X m.model.x(:,1)];
-os(end+1) = 1;
-%os(end+1:end+size(m.model.x,2))=1;
 
-X(end+1,:) = 1;
 
-% [aa,bb] = sort(os+exp(m.model.w(:)'*X(1:end-1,:)-m.model.b)', ...
-%                'descend');
-%os = os+exp(m.model.w(:)'*X(1:end-1,:)-m.model.b)';
+[w,b,alphas,pos_inds] = learn_local_capacity(X,y,index,SVMC, ...
+                                             gamma,g,m);
+%res=w'*X-b;
+%plot(res,os,'r.')
 
-%[aa,bb] = sort(m.model.w(:)'*X(1:end-1,:),'descend');
-%X = X(:,bb(1:length(bb)/10));
-%os = os(bb(1:length(bb)/10));
-[aa,bb] = sort(m.model.w(:)'*X(1:end-1,:),'descend');
-[alpha,beta] = sort(bb);
-ranks = (beta);
+m.model.w = reshape(w,m.model.hg_size);
+m.model.b = b;
+m.model.alphas = alphas;
 
-rankscores = ranks.^-.1;
-bads = find(os<.2);
-os = os .* rankscores';
-%os = rankscores';
-%os = os';
-os = reshape(os,1,[]);
-os(bads) = 0;
 
-%w = (os*inv(X'*X+lambda*eye(size(X,2),size(X,2)))*X')';
-%w2 = inv(X*X'+lambda*eye(size(X,1),size(X,1)))*X*os';
+% if 0
+%   %playing with liblinear here
+%   SVMC = 100;
 
-%alpha = pinv(X)*w;
-%alpha2 = pinv(X)*w2;
+%   y = double(os>.5);
+%   y(y==0) = -1;
+%   model = liblinear_train(y, sparse(X)', sprintf(['-s 0 -B 1 -c' ...
+%                   ' %f'],SVMC));
+%   wex = model.w(1:end-1)';
+%   b = -model.w(end);
+%   if y(1) == -1
+%     wex = wex*-1;
+%     b = b*-1;
+%   end
 
-K = X'*X;
-
-sigmoid = @(x)1./(1+exp(-x));
-lambda = 1;
-%a = fminunc(@(a)norm(sigmoid(K*a)-os').^2+lambda*norm(X*a)^2,...
-%            zeros(size(X,2),1),...
-%            optimset('MaxIter',100));
-
-%This is the real dual problem
-a = inv(K*K+lambda*K)*K*os';
-
-%get primal detector
-w = X*a;
-
-%w = fminunc(@(w)objective(w,X,os),w,optimset('MaxIter',10));
-
-m.model.b = w(end);
-m.model.w = reshape(w(1:end-1),m.model.hg_size);
-figure(2)
-clf
-imagesc(HOGpicture(reshape(m.model.w,m.model.hg_size)))
-drawnow
-
-% if exist('oldos','var')
-%   figure(3)
-%   plot(oldos,os,'r.')
-%   drawnow
+%   m.model.b = b;
+%   m.model.w = reshape(wex,m.model.hg_size);
 % end
-%oldos = os;
-
-res = w'*X(:,1:end-1);
-
-[aa,bb] = sort(res,'descend');
-
-figure(34)
-clf
-for q = 1:36
-  if q <= length(bb)
-    subplot(6,6,q)
-    I = convert_to_I(fg{imageid(bb(q))});
-    imagesc(I)
-    plot_bbox(ids{bb(q)}.bb)
-    title(num2str(aa(q)))
-    axis off
-    axis image
-  end
-end
-
-figure(444)
-plot(w'*X,os,'r.')
-drawnow
-
-keyboard
-%save m.mat m
-end
-
-function [fval,gval] = objective(w,X,os)
-lambda = 100;
-keyboard
-fval = norm(sigmoid(w'*X)-os).^2+lambda*norm(w);
-gval = 2*(sigmoid(w'*X)-os)'*sigmoidp(w'*X)*X + 2*lambda*w';
-
-function y = sigmoid(x)
-y = 1./(1+exp(-x));
-
-function y = sigmoidp(x)
-y=exp(x)./(1+exp(x)).^2;
