7bab0b5c102016ebe3210857f4821ebc14f42350
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-04-15 04:52:49 -0400

    change frame movie

--- get_movie_frame.m
+++ get_movie_frame.m
@@ -4,9 +4,8 @@ function I = get_movie_frame(movie, time)
 % move:  input move location
 % time:  a time in 00:00:00 format
 
-istring=sprintf(['ffmpeg -ss %s -i %s -vframes 1  -f' ...
-                    ' image2 /tmp/I-%%08d' '.png'],time,movie);
+istring=sprintf(['/nfs/baikal/tmalisie/ffmpeg/ffmpeg-0.6/ffmpeg -ss %s -vframes 1  -i %s ' ...
+                    ' /tmp/I-%%08d' '.png'],time,movie);
 
 [status,filer] = unix(istring);
-
 I = imread('/tmp/I-00000001.png');
