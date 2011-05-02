adbb838c4e7797f4d93678968a9ff83ff5b7d478
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-05-02 06:28:04 -0400

    new util functions

---
+++ add_new_detections.m
@@ -0,0 +1,13 @@
+function m = add_new_detections(m, xs, objids)
+%Add new detetions to the model, making sure we dont overcount duplicates
+m.model.nsv = cat(2,m.model.nsv,xs);
+m.model.svids = cat(2,m.model.svids,objids);
+
+
+names = cellfun2(@(x)sprintf('%d.%d.%d.%d.%d',x.curid,x.level, ...
+                             x.offset(1),x.offset(2),x.flip),m.model.svids);
+r = m.model.w(:)'*m.model.nsv;
+
+[unames,subset,j] = unique(names);
+m.model.svids = m.model.svids(subset);
+m.model.nsv = m.model.nsv(:,subset);
---
+++ find_set_membership.m
@@ -0,0 +1,25 @@
+function [negatives,vals,pos] = find_set_membership(m)
+%Given the objects inside m.svids, parse their curids to see which
+%of the following three sets they belong to
+bg = get_pascal_bg('trainval');
+bgtrain = get_pascal_bg('train',['-' m.cls]);
+bgval = get_pascal_bg('val',['-' m.cls]);
+fg = get_pascal_bg('trainval',m.cls);
+
+bgids = zeros(length(bg),1);
+aa = find(ismember(bg,bgtrain));
+bgids(aa) = 1;
+aa = find(ismember(bg,bgval));
+bgids(aa) = 2;
+aa = find(ismember(bg,fg));
+bgids(aa) = 3;
+
+ids = cellfun(@(x)x.curid,m.model.svids);
+%Train only with negatives
+negatives = find(ismember(ids,(find(bgids==1))));
+vals = find(ismember(ids,(find(bgids==2))));
+pos = find(ismember(ids,(find(bgids==3))));
+
+fprintf(1,'Sets NEG,VAL,POS=%d,%d,%d\n',...
+        length(negatives),length(vals),length(pos));
+
