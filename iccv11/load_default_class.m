12f1b9d963ea06dc55ced699c16dda78cec5ba5a
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-05-06 14:55:13 -0400

    now save both class and mode

--- iccv11/load_default_class.m
+++ iccv11/load_default_class.m
@@ -1,14 +1,25 @@
-function cls = load_default_class
-%Load the default object class from disk
+function [cls,mode] = load_default_class
+%Load the default object class and object mode from disk
 
 filer = '/nfs/baikal/tmalisie/default_class.txt';
 if fileexists(filer)
-  fid = fopen(filer,'r');
-  cls = fscanf(fid,'%s');
-  fclose(fid);
+  
+  cls = textread(filer,'%s');
+  mode = cls{2};
+  cls = cls{1};
+  
+  % fid = fopen(filer,'r');
+  % [cls] = fscanf(fid,'%s');
+  % fclose(fid);
+
+  %spacer = find(cls==' ');
+  %mode = cls(spacer+1:end);
+  %cls = cls(1:spacer-1);
+
   fprintf(1,'Loading default class from file "%s" ',filer);    
 else
   fprintf(1,'No default file %s, using hardcoded class ',filer);    
   cls = 'train';
+  mode = 'exemplars';
 end
-fprintf(1,'cls="%s"\n',cls);
+fprintf(1,'\n    --- cls="%s" mode="%s"\n', cls, mode);
--- iccv11/save_default_class.m
+++ iccv11/save_default_class.m
@@ -1,6 +1,8 @@
-function save_default_class(cls)
-%Save the default class to disk
+function save_default_class(cls,mode)
+%Save the default class and mode to disk
 
 fid = fopen('/nfs/baikal/tmalisie/default_class.txt','w');
 fprintf(fid,cls);
+fprintf(fid,' ');
+fprintf(fid,mode);
 fclose(fid);
