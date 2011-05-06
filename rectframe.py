4a084e3396593ad5b82afe2725e7a0d6f0cace0c
Tomasz Malisiewicz <malist@gmail.com>
2011-05-06 04:27:09 -0400

    changes

--- rectframe.py
+++ rectframe.py
@@ -11,6 +11,29 @@ class FancyFrame(wx.Frame):
         self.amount = 255;
         self.delta = -3;
         #self.SetTransparent(180)
+        self.InitializeToSize(width,height)
+        # b = wx.EmptyBitmap(width, height)
+        # dc = wx.MemoryDC()
+        # dc.SelectObject(b)
+        # dc.SetBackground(wx.Brush('black'))
+        # dc.Clear()
+        # dc.SetBrush(wx.TRANSPARENT_BRUSH)
+        # dc.SetPen(wx.Pen('red', 4))
+        # dc.DrawRectangle(10, 10, width-20, height-20)
+        # dc.SelectObject(wx.NullBitmap)
+        # b.SetMaskColour('black')
+        # self.SetShape(wx.RegionFromBitmap(b))
+
+        
+        # self.SetBackgroundColour('red')
+        # self.Show(True)
+
+        self.Bind(wx.EVT_KEY_UP, self.OnKeyDown)
+        self.timer = wx.Timer(self)
+        self.timer.Start(25)
+        self.Bind(wx.EVT_TIMER, self.AlphaCycle)
+
+    def InitializeToSize(self,width,height):
         b = wx.EmptyBitmap(width, height)
         dc = wx.MemoryDC()
         dc.SelectObject(b)
@@ -22,22 +45,16 @@ class FancyFrame(wx.Frame):
         dc.SelectObject(wx.NullBitmap)
         b.SetMaskColour('black')
         self.SetShape(wx.RegionFromBitmap(b))
-
-        self.Bind(wx.EVT_KEY_UP, self.OnKeyDown)
         self.SetBackgroundColour('red')
         self.Show(True)
-
-        self.timer = wx.Timer(self)
-        self.timer.Start(25)
-        self.Bind(wx.EVT_TIMER, self.AlphaCycle)
-
+        
     def AlphaCycle(self, evt):
         self.amount += self.delta
         if self.amount == 0 or self.amount == 255:
             self.delta = -self.delta
         self.Move((self.amount,250))
         #print self.amount
-        #self.SetSize((self.amount+100,220))
+        self.InitializeToSize(self.amount+100,220)
         self.Show()
 
 
