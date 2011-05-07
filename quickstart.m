ddc5e3bbab0af2216473189b3dfaf7b4d1dae8a7
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-05-07 01:50:47 -0400

    we run experiments overnight!

--- quickstart.m
+++ quickstart.m
@@ -1,11 +1,11 @@
-%%% training a cow detector
+classes = {'train','tvmonitor','bottle','cow'};
 
-%1. dump out model files for a VOC category
-%initialize_voc_exemplars('cow');
+for i = 1:length(classes)
+%% Here is the main script for doing the NN baseline
 
-%Set default class, by writing into the default file, which our
-%mapreduces will be reading.
-save_default_class('car');
+%Set default class and detector mode, by writing into the default
+%file, which our mapreduces will be reading.
+save_default_class(classes{i},'exemplars-dt');
 
 %Run an "exemplar_initialize" mapreduce
 timing.initialize = spawn_job('ei',50,2);
@@ -14,30 +14,12 @@ timing.initialize = spawn_job('ei',50,2);
 models = load_all_models;
 
 %Run an "apply_voc_exemplars"
-timing.apply = spawn_job('ave',80,2);
+timing.apply = spawn_job('ave',120,2);
 
-models = load_all_models;
 grid = load_result_grid(models);
 clear M
 M.betas = perform_calibration(models,grid);
 results = evaluate_pascal_voc_grid(models, grid, 'test', M);
+results = evaluate_pascal_voc_grid(models, grid, 'test');
 
-%2. train all exemplars (all exemplars in exemplar directory)
-% make sure mining_params has dump_images enabled if you want to
-% look at the exemplars
-%mining_params.dump_images = 1;
-%train_all_exemplars;
-
-return
-
-%3. perform calibration
-models = load_all_exemplars;
-grid = load_result_grid(models);
-clear M
-M.betas = perform_calibration(grid,models);
-
-%4. evaluate results on PASCAL VOC comp3 task
-results = evaluate_pascal_voc_grid(models, grid, 'test', M);
-
-%5. show top detections on testset
-show_top_dets(models,grid,'test',betas);
+end
