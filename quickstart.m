06046a9762185391d8e5d056e7505887fc7d0fe3
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-05-11 13:06:58 -0400

    change quickstart directions

--- quickstart.m
+++ quickstart.m
@@ -1,11 +1,13 @@
-classes = {'train','tvmonitor','bottle','cow'};
+%classes =...
+%{'car','dog','cat','bird','chair','pottedplant'};%'tvmonitor','bottle','cow'};
+classes = {'train'};
 
 for i = 1:length(classes)
 %% Here is the main script for doing the NN baseline
 
 %Set default class and detector mode, by writing into the default
-%file, which our mapreduces will be reading.
-save_default_class(classes{i},'exemplars-dt');
+%file, which our mao,2n-.0mpreduces will be reading.
+save_default_class(classes{i},'exemplars-ncc');
 
 %Run an "exemplar_initialize" mapreduce
 timing.initialize = spawn_job('ei',50,2);
@@ -14,12 +16,13 @@ timing.initialize = spawn_job('ei',50,2);
 models = load_all_models;
 
 %Run an "apply_voc_exemplars"
-timing.apply = spawn_job('ave',120,2);
+timing.apply = spawn_job('ave',80,2);
 
 grid = load_result_grid(models);
-clear M
-M.betas = perform_calibration(models,grid);
-results = evaluate_pascal_voc_grid(models, grid, 'test', M);
-results = evaluate_pascal_voc_grid(models, grid, 'test');
+%clear M
+%M.betas = perform_calibration(models,grid);
+%[results,final] = evaluate_pascal_voc_grid(models, grid, 'test', M);
+[results,final] = evaluate_pascal_voc_grid(models, grid, 'test');
 
+show_top_transfers(models,grid,'test',final)
 end
