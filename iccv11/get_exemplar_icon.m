cac6eb263f12dfc86c0c2ff695eea3f2ac66fe87
Tomasz Malisiewicz <malist@gmail.com>
2011-05-16 03:54:29 -0400

    change what is displayed

--- iccv11/get_exemplar_icon.m
+++ iccv11/get_exemplar_icon.m
@@ -1,5 +1,5 @@
 function [Iex,alphamask] = get_exemplar_icon(models,index,flip)
-%given a cell array of models, show model at index
+%given a cell array of models, show model at index (gt version of bb)
 
 if ~exist('flip','var')
   flip = 0;
@@ -10,36 +10,41 @@ end
 
 if isfield(models{index},'I')
   Iex = models{index}.I;
+  cb = models{index}.gt_box;    
+    %cb = models{index}.model.coarse_box;
+    
+    PADDER = 100;
+    Iex = pad_image(Iex, PADDER);
+    cb = round(cb + PADDER);
+    Iex = Iex(cb(2):cb(4),cb(1):cb(3),:);
+    
 else
 
-
-try
-  VOCinit;
-  Iex = im2double(imread(sprintf(VOCopts.imgpath, ...
-                                 models{index}.curid)));
-  %fprintf(1,'warning using coarse box\n');
-  cb = models{index}.gt_box;
-
-  %cb = models{index}.model.coarse_box;
-
-  PADDER = 100;
-
-  Iex = pad_image(Iex, PADDER);
-  cb = round(cb + PADDER);
-  Iex = Iex(cb(2):cb(4),cb(1):cb(3),:);
-  
-catch
-  %Iex = zeros(100,100,3);
-  Iex = HOGpicture(models{index}.model.w);
-  Iex = Iex - min(Iex(:));
-  Iex = Iex / max(Iex(:));
-  
-  colors = jet(100);
-  c = colors(round(Iex(:)*99+1),:);
-  c = reshape(c,[size(Iex,1) ...
-                    size(Iex,2) 3]);
-  Iex = c;
-end
+  try
+    VOCinit;
+    Iex = im2double(imread(sprintf(VOCopts.imgpath, ...
+                                   models{index}.curid)));
+    %fprintf(1,'warning using coarse box\n');
+    cb = models{index}.gt_box;    
+    %cb = models{index}.model.coarse_box;
+    
+    PADDER = 100;
+    Iex = pad_image(Iex, PADDER);
+    cb = round(cb + PADDER);
+    Iex = Iex(cb(2):cb(4),cb(1):cb(3),:);
+    
+  catch
+    %Iex = zeros(100,100,3);
+    Iex = HOGpicture(models{index}.model.w);
+    Iex = Iex - min(Iex(:));
+    Iex = Iex / max(Iex(:));
+    
+    colors = jet(100);
+    c = colors(round(Iex(:)*99+1),:);
+    c = reshape(c,[size(Iex,1) ...
+                   size(Iex,2) 3]);
+    Iex = c;
+  end
 end
 
 
