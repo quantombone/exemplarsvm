e99987d79eb11bde2aa227552906f2724e5daae1
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-05-28 00:45:30 -0400

    reduced interface now

--- iccv11/extract_bbs_from_rs.m
+++ iccv11/extract_bbs_from_rs.m
@@ -1,4 +1,4 @@
-function [bboxes] = extract_bbs_from_rs(rs, index)
+function [bboxes] = extract_bbs_from_rs(svids, svscores, index)
 %% Given a resultstruct 'rs' from a localizemeHOG function and an
 %% image index, convert the results into one large result matrix
 %% which stores the bb's
@@ -7,21 +7,21 @@ function [bboxes] = extract_bbs_from_rs(rs, index)
 %% bboxes(i,:) = [1  2  3  4  5 6    7    8     9  10 11    12];
 %% Tomasz Malisiewicz (tomasz@cmu.edu)
 
-bboxes = cell(length(rs.id_grid),1);
-for j = 1:length(rs.id_grid)
-  if length(rs.id_grid{j})==0
+bboxes = cell(length(svids),1);
+for j = 1:length(svids)
+  if length(svids{j})==0
     continue
   end
-  bbs = cellfun2(@(x)x.bb,rs.id_grid{j});
+  bbs = cellfun2(@(x)x.bb,svids{j});
   bbs = cat(1,bbs{:});
   bbs(:,5:12) = 0;
   bbs(:,6) = j;
-  bbs(:,7) = cellfun(@(x)x.flip,[rs.id_grid{j}])';
-  bbs(:,8) = cellfun(@(x)x.scale,[rs.id_grid{j}])';
-  bbs(:,9) = cellfun(@(x)x.offset(1),[rs.id_grid{j}])';
-  bbs(:,10) = cellfun(@(x)x.offset(2),[rs.id_grid{j}])';
+  bbs(:,7) = cellfun(@(x)x.flip,[svids{j}])';
+  bbs(:,8) = cellfun(@(x)x.scale,[svids{j}])';
+  bbs(:,9) = cellfun(@(x)x.offset(1),[svids{j}])';
+  bbs(:,10) = cellfun(@(x)x.offset(2),[svids{j}])';
   bbs(:,11) = index;
-  bbs(:,12) = rs.score_grid{j}';
+  bbs(:,12) = svscores{j}';
   
   bboxes{j} = bbs;
 end
