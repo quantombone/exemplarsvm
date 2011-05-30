905ffcb8a98a7c07393001b8e4ce03800d827818
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-05-29 20:09:51 -0400

    keep original placement bb in stripped models

--- nips11/strip_model.m
+++ nips11/strip_model.m
@@ -1,8 +1,8 @@
 function models = strip_model(models)
 for i = 1:length(models)
   models{i}.model.x = [];
-  models{i}.model.target_id = [];
-  models{i}.model.target_x = [];
+  models{i}.model.target_id = models{i}.model.target_id(1);
+  models{i}.model.target_x = models{i}.model.target_x(:,1);
 
   %models{i}.model.svscores = models{i}.model.w(:)'*models{i}.model.nsv ...
   %    - models{i}.model.b;
