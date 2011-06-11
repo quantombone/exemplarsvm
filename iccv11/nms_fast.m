9881c91b0209ca0de1dd69908da3e024a501aaa5
Tomasz Malisiewicz <malist@gmail.com>
2011-06-11 17:27:49 -0400

    fast is fast

--- iccv11/nms_fast.m
+++ iccv11/nms_fast.m
@@ -16,26 +16,29 @@ y2 = boxes(:,4);
 s = boxes(:,end);
 
 area = (x2-x1+1) .* (y2-y1+1);
-
 [vals, I] = sort(s);
-pick = [];
 
+pick = s*0;
+counter = 1;
 while ~isempty(I)
   
   last = length(I);
   i = I(last);  
-  pick = [pick; i];
+  pick(counter) = i;
+  counter = counter + 1;
   
   xx1 = max(x1(i), x1(I(1:last-1)));
   yy1 = max(y1(i), y1(I(1:last-1)));
   xx2 = min(x2(i), x2(I(1:last-1)));
   yy2 = min(y2(i), y2(I(1:last-1)));
+  
   w = max(0.0, xx2-xx1+1);
   h = max(0.0, yy2-yy1+1);
   
   o = w.*h ./ area(I(1:last-1));
   
-  I([last; find(o>.5)]) = [];
+  I([last; find(o>overlap)]) = [];
 end
 
+pick = pick(1:(counter-1));
 top = boxes(pick,:);
