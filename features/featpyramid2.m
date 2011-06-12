1dc93f7c324a9b7cc01d21085c49ae75d037cb38
Tomasz Malisiewicz <malist@gmail.com>
2011-06-11 20:10:22 -0400

    pyramid code now allows for different levels of application

--- features/featpyramid2.m
+++ features/featpyramid2.m
@@ -1,55 +1,37 @@
 function [feat, scale] = featpyramid2(im, sbin, interval)
-
-% [feat, scale] = featpyramid(im, sbin, interval);
-% Compute feature pyramid.
-%
-% sbin is the size of a HOG cell - it should be even.
-% interval is the number of scales in an octave of the pyramid.
-% feat{i} is the i-th level of the feature pyramid.
-% scale{i} is the scaling factor used for the i-th level.
-% feat{i+interval} is computed at exactly half the resolution of feat{i}.
-% first octave halucinates higher resolution data.
-
-%% here we pad data to always be a multiple of the bin
+% [feat, scale] = featpyramid2(im, sbin, interval);
 
 sc = 2 ^(1/interval);
-imsize = [size(im, 1) size(im, 2)];
-%max_scale = 1 + floor(log(min(imsize)/(5*sbin))/log(sc));
-%feat = cell(max_scale + interval, 1);
-%scale = zeros(max_scale + interval, 1);
-%pads = cell(max_scale + interval, 1);
-% our resize function wants floating point values
+
 im = double(im);
 
-DOD = 1;
-for i = 1:200
-  scaler = 1/sc^(i-1);
+MAXSCALE = 1.0;
+MINSCALE = .01;
 
-  if scaler < .01
+MAXLEVELS = 200;
+MINDIMENSION = 5;
+
+% Start at MAXSCALE, and keep going down by the increment sc, until
+% we reach MAXLEVELS or MINSCALE
+for i = 1:MAXLEVELS
+  scaler = MAXSCALE / sc^(i-1);
+  
+  if scaler < MINSCALE
     return
   end
   
   scale(i) = scaler;
-
+  scaled = resize(im,scale(i));
   
-  if (DOD && (scale(i) > 1))
-    scaled = resize(im,scale(i)/2);
-  else
-    scaled = resize(im,scale(i));
-  end
-  
-  
-  
-  if min([size(scaled,1) size(scaled,2)])<=5
+  %if minimum dimensions is less than or equal to 5, exit
+  if min([size(scaled,1) size(scaled,2)])<=MINDIMENSION
     return
   end
-  if (DOD && (scale(i) > 1))
-    feat{i} = features(scaled,sbin/2);
-  else
-    feat{i} = features(scaled,sbin);
-  end
 
-  %if we get zero size feature, backtrack one
+  feat{i} = features(scaled,sbin);
+
+  %if we get zero size feature, backtrack one, and dont produce any
+  %more levels
   if (size(feat{i},1)*size(feat{i},2)) == 0
     feat = feat(1:end-1);
     scale = scale(1:end-1);
@@ -58,22 +40,11 @@ for i = 1:200
 
   %recover lost bin!!!
   feat{i} = padarray(feat{i}, [1 1 0], 0);
-  
-  if max([size(feat{i},1) size(feat{i},2)])<=5
+
+  %if the max dimensions is less than or equal to 5, dont produce
+  %any more levels
+  if max([size(feat{i},1) size(feat{i},2)])<=MINDIMENSION
     return;
   end
   
 end
-
-% function [im2,pads] = scale_before_resize(im,sbin,scale)
-% %doesnt help really
-% offer = ceil([size(im,1) size(im,2)] * (scale/sbin)) / (scale/ ...
-%                                                   sbin);
-
-% pads = offer - [size(im,1) size(im,2)];
-
-% pads = round(pads);
-% im2 = im;
-% im2(end+1:end+pads(1),end+1:end+pads(2),:)=0;
-
-% im2 = resize(im2,scale);
