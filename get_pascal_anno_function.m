d4ecb335a459a54f4e2df2113880a6ce9fd86fe1
Tomasz Malisiewicz <malist@gmail.com>
2011-06-16 02:54:19 -0400

    get the anno function

---
+++ get_pascal_anno_function.m
@@ -0,0 +1,18 @@
+function extras = get_pascal_anno_function(dataset_params, Iname, boxes)
+
+[tmp,curid,tmp] = fileparts(Iname);
+
+% get GT objects for this image
+recs = PASreadrecord(sprintf(dataset_params.annopath,curid));
+
+% get overlaps with all ground-truths (makes sense for VOC
+% images only)
+gtbb = cat(1,recs.objects.bbox);
+os = getosmatrix_bb(boxes,gtbb);
+cats = {recs.objects.class};
+[tmp,cats] = ismember(cats,dataset_params.classes);
+
+[alpha,beta] = max(os,[],2);
+extras.maxos = alpha;
+extras.maxind = beta;
+extras.maxclass = cats(beta);
