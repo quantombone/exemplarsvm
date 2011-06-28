5bb25e895339a7554ad1cd5c5e7b1f002eb85a8b
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-06-28 15:09:09 -0400

    re-arrange input parameters

--- iccv11/get_pascal_bg.m
+++ iccv11/get_pascal_bg.m
@@ -1,20 +1,21 @@
-function bg = get_pascal_bg(target_directory, class, VOCopts)
-%% Get a PASCAL dataset from the 'train','trainval', or 'test' set
-%% if class is "motorcycle" choose only images containing motorcycle
-%% if class is "-car" chose only images not containing cars
+function bg = get_pascal_bg(VOCopts, target_directory, class)
+% Get a PASCAL dataset from the 'train', 'trainval', or 'test' set
+% of the VOCYEAR dataset as defined in VOCopts
+% if class is "motorcycle" choose only images containing motorcycle
+% if class is "-car" chose only images not containing cars
 
-%% eliminating images with id kill_image_id (if present)
-
-if nargin ==1 && strcmp(target_directory,'both')
-  bg = cat(1,get_pascal_bg('trainval'),get_pascal_bg('test'));
+if (nargin == 2) && strcmp(target_directory,'both')
+  bg = cat(1, ...
+           get_pascal_bg(VOCopts, 'trainval'),...
+           get_pascal_bg(VOCopts, 'test'));
   return;  
 end
 
-%VOCinit;
 if ~exist('target_directory','var');
   target_directory = 'train';
 end
 
+%% eliminating images with id kill_image_id (if present)
 if ~exist('kill_image_id','var')
   kill_image_id = '';
 end
