258d8b1b74a44f5a38f593d303af3eb5153996be
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-07-01 05:25:49 -0400

    big run for the goalsize framing strategy

--- scripts/voc_seg_all_gf.m
+++ scripts/voc_seg_all_gf.m
@@ -14,16 +14,21 @@ dataset_params.model_type = 'exemplar';
 
 %Initialize framing function
 init_params.sbin = 8;
-init_params.goal_ncells = 200;
-init_params.MAXDIM = 15;
+init_params.goal_ncells = 100;
+init_params.MAXDIM = 12;
 init_params.init_function = @initialize_goalsize_model;
-init_params.init_type = 'f';
+init_params.init_type = sprintf('g-%d-%d',...
+                                init_params.goal_ncells,...
+                                init_params.MAXDIM);
 
 % %Initialize framing function
 % init_params.sbin = 8;
 % init_params.hg_size = [8 8];
 % init_params.init_function = @initialize_fixedframe_model;
-% init_params.init_type = 'f';
+% init_params.init_type = sprintf('f-%d-%d',...
+%                                 init_params.hg_size(1),...
+%                                 init_params.hg_size(2));
+
 
 %Choose the training function (do_svm, do_rank, ...)
 training_function = @do_svm;
@@ -49,16 +54,16 @@ dataset_params.mining_params = mining_params;
 
 %Initialize exemplar stream
 dataset_params.stream_set_name = 'trainval';
-dataset_params.stream_max_ex = 50;
+dataset_params.stream_max_ex = 2000;
 
 dataset_params.trainset_name = 'train';
-dataset_params.trainset_maxk = 100;
+dataset_params.trainset_maxk = 2000;
 
 dataset_params.valset_name = 'trainval';
-dataset_params.valset_maxk = 20;
+dataset_params.valset_maxk = 10000;
 
 dataset_params.testset_name = 'test';
-dataset_params.testset_maxk = 2000;
+dataset_params.testset_maxk = 10000;
 
 %Choose a short string to indicate the type of training run we are doing
 dataset_params.models_name = ...
@@ -69,6 +74,12 @@ dataset_params.models_name = ...
 
 classes = {...
     'bus'
+    'cow'
+    'train'
+    'motorbike'
+    'dog'
+    'sofa'
+    'boat'
 };
 
 myRandomize;
@@ -76,9 +87,12 @@ r = randperm(length(classes));
 classes = classes(r);
 
 for i = 1:length(classes)
+  %Training set is negatives
   dataset_params.trainset_name2 = ['-' classes{i}];
-  dataset_params.valset_name2 = ['-' classes{i}];
-  dataset_params.testset_name2 = classes{i};
+  %Validation set is all of trainval
+  dataset_params.valset_name2 = '';%['-' classes{i}];
+  %Test-set is all of testing
+  dataset_params.testset_name2 = '';%classes{i};
   
   voc_template(dataset_params, classes{i});
 end
