05fd39656fa5d409d93de8f3f59258bfd55f146a
Tomasz Malisiewicz <malist@gmail.com>
2011-07-01 14:24:43 -0700

    updated params

--- get_default_mining_params.m
+++ get_default_mining_params.m
@@ -29,9 +29,6 @@ mining_params.dump_images = 0;
 %if enabled, we dump the last image
 mining_params.dump_last_image = 1;
 
-%maximum #windows per image (per exemplar) to mine
-%mining_params.MAX_WINDOWS_PER_IMAGE = 100;
-
 %Levels-per-octave defines how many levels between 2x sizes in pyramid
 mining_params.lpo = 10;
 
@@ -40,6 +37,8 @@ mining_params.SAVE_SVS = 0;
 
 %default detection threshold
 mining_params.thresh = -1;
+
+%maximum #windows per image (per exemplar) to mine
 mining_params.TOPK = 10;
 
 %How much we pad the pyramid (to let detections fall outside the image)
@@ -49,18 +48,8 @@ mining_params.pyramid_padder = 5;
 %defines one iteration)
 mining_params.MAX_WINDOWS_BEFORE_SVM = 1000;
 
-%Maximum number of violating images before SVM kicks in (another
-%way to define one iteration)
-%NOTE: this variable breaks how I consider iterations, so make it
-%really large so it never fires
+%Maximum number of violating images before SVM is trained with current cache
 mining_params.MAX_IMAGES_BEFORE_SVM = 400;
-
-%At this cutoff, we switch thresholds from basically everything to
-%a reduced threshold
-%mining_params.early_late_cutoff = 3;
-%mining_params.early_detection_threshold = -10000;
-%mining_params.late_detection_threshold = -1.05;
-%mining_params.detection_threshold = mining_params.late_detection_threshold;
 
 %when mining, we keep the N negative support vectors as well as
 %some more beyond the -1 threshold (alpha*N), but no more than 1000
@@ -69,10 +58,10 @@ mining_params.max_negatives = 2000;
 
 %%if less than 1.0, then we apply nms to detections so that we don't have
 %%too many redundant windows
-mining_params.NMS_MINES_OS = 0.5;
+mining_params.NMS_MINES_OS = 1.0;
 
 %if non-zero, then skip detection at any objects
-mining_params.SKIP_GTS_ABOVE_THIS_OS = 10.0;
+%mining_params.SKIP_GTS_ABOVE_THIS_OS = 10.0;
 
 %if non-zero, add these detections as positives
 %mining_params.ASSIMILATE_GTS_ABOVE_THIS_OS = 10;
@@ -87,10 +76,8 @@ mining_params.BALANCE_POSITIVES = 0;
 % if non-zero, perform learning in dominant-gradient space
 mining_params.DOMINANT_GRADIENT_PROJECTION = 0;
 mining_params.DOMINANT_GRADIENT_PROJECTION_K = 2;
-
 mining_params.DO_PCA = 0;
 mining_params.PCA_K = 300;
-
 mining_params.A_FROM_POSITIVES = 0;
 
 %if enabled, we must extract training negatives from positives and
@@ -105,3 +92,5 @@ mining_params.skip_mine = 0;
 %if greater than 0, then only keep dets that have this OS with the
 %entire test image
 mining_params.MIN_SCENE_OS = 0.0;
+mining_params.MAXSCALE = 1.0;
+mining_params.MINSCALE = .01;
