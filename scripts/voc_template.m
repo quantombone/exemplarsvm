8bc986160c1c865ceddc3c33b2891fb5581e9933
Tomasz Malisiewicz <malist@gmail.com>
2011-07-02 15:31:44 -0400

    re-organize the template

--- scripts/voc_template.m
+++ scripts/voc_template.m
@@ -1,24 +1,62 @@
 function voc_template(dataset_params, cls)
 %% This is the main VOC driver script for both scenes and exemplars
 
+models_name = dataset_params.models_name;
+
+%% Initialize exemplars with the exemplar stream
 e_stream_set = get_pascal_stream(dataset_params, cls);
+efiles = exemplar_initialize(dataset_params, e_stream_set, ...
+                             models_name, dataset_params.init_params);
 
-train_set = get_pascal_set(dataset_params, ...
-                           dataset_params.trainset_name,...
-                           dataset_params.trainset_name2);
-train_set = train_set(1:min(length(train_set), dataset_params.trainset_maxk));
+%Load all of the initialized exemplars
+models = load_all_models(dataset_params, cls, models_name, efiles, 1);
 
-val_set = get_pascal_set(dataset_params, ...
-                         dataset_params.valset_name,...
-                         dataset_params.valset_name2);
-val_set = val_set(1:min(length(val_set), dataset_params.valset_maxk));
+%% Train each initialized exemplar 
+if (dataset_params.SKIP_TRAINING == 0)
+  train_set = get_pascal_set(dataset_params, ...
+                             dataset_params.trainset_name,...
+                             dataset_params.trainset_name2);
+  train_set = train_set(1:min(length(train_set), ...
+                              dataset_params.trainset_maxk));
+  
 
-if strcmp(dataset_params.model_type,'exemplar')
-  val_gt_function = @get_pascal_anno_function;
-else
-  val_gt_function = [];
+  [tfiles, models_name] = train_all_exemplars(dataset_params, models, ...
+                                              train_set, ...
+                                              dataset_params.mining_params, ...
+                                              dataset_params.training_function);
+  
+  %Load the trained exemplars
+  models = load_all_models(dataset_params, cls, models_name, tfiles, ...
+                           1, 1);
+  
 end
-val_params = get_default_mining_params;
+
+if strcmp(dataset_params.model_type,'exemplar') && ...
+      (dataset_params.SKIP_VAL == 0)
+
+  val_set = get_pascal_set(dataset_params, ...
+                           dataset_params.valset_name,...
+                           dataset_params.valset_name2);
+  val_set = val_set(1:min(length(val_set), dataset_params.valset_maxk));
+  
+  val_gt_function = @get_pascal_anno_function;
+  val_params = get_default_mining_params;
+
+  %% Apply trained exemplars on validation set
+  val_files = apply_all_exemplars(dataset_params, models, val_set, ...
+                                  dataset_params.valset_name, ...
+                                  [], val_params, val_gt_function);
+
+  %Load validation results
+  val_grid = load_result_grid(dataset_params, models, ...
+                              dataset_params.valset_name, val_files);
+  
+  %% Perform l.a.b.o.o. calibration and M-matrix estimation
+  M = calibrate_and_estimate_M(dataset_params, models, val_grid);
+  
+else
+  M = [];
+end
 
 test_set = get_pascal_set(dataset_params, ...
                           dataset_params.testset_name,...
@@ -32,26 +70,6 @@ if length(test_set) == 0
   fprintf(1,'Warning, testset is empty\n');
   return;
 end
-
-models_name = dataset_params.models_name;
-init_params = dataset_params.init_params;
-
-%% Initialize exemplars with the exemplar stream
-efiles = exemplar_initialize(dataset_params, e_stream_set, ...
-                             models_name, init_params);
-
-%Load all of the initialized exemplars
-models = load_all_models(dataset_params, cls, models_name, efiles, 1);
-
-%% Train each initialized exemplar
-[tfiles, models_name] = train_all_exemplars(dataset_params, models, ...
-                                            train_set, ...
-                                            dataset_params.mining_params, ...
-                                            dataset_params.training_function);
-
-%Load the trained exemplars
-models = load_all_models(dataset_params, cls, models_name, tfiles, ...
-                         1, 1);
 
 %% Apply trained exemplars on test set
 test_files = apply_all_exemplars(dataset_params, models, test_set, ...
@@ -82,27 +100,7 @@ show_memex_browser(dataset_params, models, test_grid,...
 %               test_set, dataset_params.testset_name, ...
 %               teststruct);
 
-if dataset_params.SKIP_VALIDATION == 1
-  return;
-end
 
-
-if strcmp(dataset_params.model_type,'exemplar')
-  %% Apply trained exemplars on validation set
-  val_files = apply_all_exemplars(dataset_params, models, val_set, ...
-                                  dataset_params.valset_name, ...
-                                  [], val_params, val_gt_function);
-
-  %Load validation results
-  val_grid = load_result_grid(dataset_params, models, ...
-                              dataset_params.valset_name, val_files);
-  
-  %% Perform l.a.b.o.o. calibration and M-matrix estimation
-  M = calibrate_and_estimate_M(dataset_params, models, val_grid);
-  
-else
-  M = [];
-end
 
 if length(M) > 0 && (dataset_params.SKIP_EVAL == 0)
   %% Evaluation of l.a.b.o.o. afer training
