fb94d169101658c9b7e4670bfdf1a78863fb955e
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-07-05 16:28:13 -0400

    fix a bug with these params pointing to wrong set

--- get_default_mining_params_scene.m
+++ get_default_mining_params_scene.m
@@ -1,5 +1,5 @@
 function mining_params = get_default_mining_params_scene
 mining_params = get_default_mining_params;
 
-val_params.MAXSCALE = .4;
-val_params.MIN_SCENE_OS = 0.4;
+mining_params.MAXSCALE = .4;
+mining_params.MIN_SCENE_OS = 0.4;
