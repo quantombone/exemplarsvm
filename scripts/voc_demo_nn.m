ac6a462cac2f5cf0574cbee5ff1308d8974a2dd2
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-07-05 16:29:39 -0400

    re-arrange placement of some parameters

--- scripts/voc_demo_nn.m
+++ scripts/voc_demo_nn.m
@@ -2,13 +2,18 @@ clear;
 
 %% Initialize dataset
 VOCYEAR = 'VOC2007';
-suffix = '/nfs/baikal/tmalisie/demo11/';
+suffix = '/nfs/baikal/tmalisie/nn11/';
 dataset_params = get_voc_dataset(VOCYEAR,suffix);
 dataset_params.display = 0;
 
-dataset_params.must_have_seg = 0;
-dataset_params.must_have_seg_string = '';
-dataset_params.model_type = 'exemplar';
+%SKIP_TRAINING = 1;
+%SKIP_VAL = 0;
+
+dataset_params.SKIP_EVAL = 0;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% SET SOURCETRAIN/VAL/TEST PARAMS %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Initialize framing function
 % init_params.sbin = 8;
@@ -19,44 +24,59 @@ dataset_params.model_type = 'exemplar';
 %                                 init_params.goal_ncells,...
 %                                 init_params.MAXDIM);
 
-%Initialize framing function
+%Initialize exemplar framing function
 init_params.sbin = 8;
 init_params.hg_size = [8 8];
 init_params.init_function = @initialize_fixedframe_model;
+init_params.init_string = 'f';
 init_params.nnmode = 1;
-init_params.init_type = sprintf('nn-%d-f-%d-%d', ...
-                                init_params.nnmode, ...
+init_params.nn_string = 'nn-1';
+init_params.init_type = sprintf('%s-%s-%d-%d', ...
+                                init_params.nn_string, ...
+                                init_params.init_string, ...
                                 init_params.hg_size(1), ...
                                 init_params.hg_size(2));
 
 dataset_params.init_params = init_params;
 
-dataset_params.params = get_default_mining_params;
-dataset_params.mining_params = dataset_params.params;
-
-%Choose the training function (do_svm, do_rank, ...)
-dataset_params.mining_params.training_function = @do_svm;
-
-%Disable NMS in training params
-dataset_params.mining_params.NMS_OS = 1.0;
-
-dataset_params.val_params = dataset_params.params;
-dataset_params.test_params = dataset_params.params;
-
 %Initialize exemplar stream
 dataset_params.stream_set_name = 'trainval';
 dataset_params.stream_max_ex = 2000;
 
-dataset_params.trainset_name = 'train';
-dataset_params.trainset_maxk = 0;
+dataset_params.must_have_seg = 0;
+dataset_params.must_have_seg_string = '';
+dataset_params.model_type = 'exemplar';
 
-dataset_params.valset_name = 'trainval';
-dataset_params.valset_maxk = 0;
+%Create mining/validation/testing params as defaults
+dataset_params.params = get_default_mining_params;
 
-dataset_params.testset_name = 'test';
-dataset_params.testset_maxk = 1000;
+if 0
+  %Choose the training function (do_svm, do_rank, ...)
+  %Disable NMS in training params
+  dataset_params.mining_params = dataset_params.params;
+  dataset_params.mining_params.training_function = @do_svm;
+  dataset_params.mining_params.NMS_OS = 1.0;
+  dataset_params.mining_params.MAXSCALE = 0.5;
+  dataset_params.mining_params.set_name = 'train';
+  %optional cap
+  %dataset_params.mining_params.set_maxk = 0;
+end
 
+if 0
+  dataset_params.val_params = dataset_params.params;
+  dataset_params.val_params.NMS_OS = 0.5;
+  dataset_params.val_params.set_name = 'trainval';
+  %optional cap
+  %dataset_params.val_params.set_maxk = 10000;
+end
 
+if 1
+  dataset_params.test_params = dataset_params.params;
+  dataset_params.test_params.NMS_OS = 0.5;
+  dataset_params.test_params.set_name = 'test';
+  %optional cap
+  %dataset_params.test_params.set_maxk = 0;
+end
 %Choose a short string to indicate the type of training run we are doing
 dataset_params.models_name = ...
     [init_params.init_type ...
@@ -64,31 +84,28 @@ dataset_params.models_name = ...
      '.' ...
      dataset_params.model_type];
 
-dataset_params.SKIP_TRAINING = 1;
-dataset_params.SKIP_VAL = 1;
-dataset_params.SKIP_EVAL = 1;
-
-classes = {...
-    'aeroplane'
-    'bicycle'
-    'bird'
-    'boat'
-    'bottle'
-    'bus'
-    'car'
-    'cat'
-    'chair'
-    'cow'
-    'diningtable'
-    'dog'
-    'horse'
-    'motorbike'
-    'pottedplant'
-    'sheep'
-    'sofa'
-    'train'
-    'tvmonitor'
-};
+classes = {'bus'};
+% classes = {...
+%     'aeroplane'
+%     'bicycle'
+%     'bird'
+%     'boat'
+%     'bottle'
+%     'bus'
+%     'car'
+%     'cat'
+%     'chair'
+%     'cow'
+%     'diningtable'
+%     'dog'
+%     'horse'
+%     'motorbike'
+%     'pottedplant'
+%     'sheep'
+%     'sofa'
+%     'train'
+%     'tvmonitor'
+% };
 
 myRandomize;
 r = randperm(length(classes));
@@ -96,13 +113,21 @@ classes = classes(r);
 
 for i = 1:length(classes)
   %Training set is negatives
-  dataset_params.trainset_name2 = ['-' classes{i}];
-  
-  %Validation set is in-class valset
-  dataset_params.valset_name2 = [classes{i}];
-  
-  %Test-set is in-class testset
-  dataset_params.testset_name2 = ''; %classes{i};
-  
+  if isfield(dataset_params,'mining_params')
+    dataset_params.mining_params.set_name2 = ['-' classes{i}];
+  end
+
+  if isfield(dataset_params,'val_params')
+    dataset_params.val_params.set_name2 = '';
+    %Validate on in-class images only
+    dataset_params.val_params.set_name2 = classes{i};;
+  end
+
+  if isfield(dataset_params,'test_params')
+    dataset_params.test_params.set_name2 = '';
+    %Test on in-class images only
+    dataset_params.test_params.set_name2 = classes{i};;
+  end
+    
   voc_template(dataset_params, classes{i});
 end
--- scripts/voc_template.m
+++ scripts/voc_template.m
@@ -3,18 +3,6 @@ function voc_template(dataset_params, cls)
 
 models_name = dataset_params.models_name;
 
-if ~isfield(dataset_params,'train_params')
-  dataset_params.train_params = dataset_params.params;
-end
-
-if ~isfield(dataset_params,'val_params')
-  dataset_params.val_params = dataset_params.params;
-end
-
-if ~isfield(dataset_params,'test_params')
-  dataset_params.test_params = dataset_params.params;
-end
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%% EXEMPLAR INITIALIZATION %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -25,7 +13,7 @@ efiles = exemplar_initialize(dataset_params, e_stream_set, ...
                              models_name, dataset_params.init_params);
 
 %Load all of the initialized exemplars
-CACHE_FILE = 0;
+CACHE_FILE = 1;
 STRIP_FILE = 0;
 models = load_all_models(dataset_params, cls, models_name, ...
                          efiles, CACHE_FILE, STRIP_FILE);
@@ -36,21 +24,29 @@ models = load_all_models(dataset_params, cls, models_name, ...
 
 
 %% Train each initialized exemplar 
-if (dataset_params.SKIP_TRAINING == 0)
-  train_set = get_pascal_set(dataset_params, ...
-                             dataset_params.trainset_name,...
-                             dataset_params.trainset_name2);
-  train_set = train_set(1:min(length(train_set), ...
-                              dataset_params.trainset_maxk));
+if isfield(dataset_params,'mining_params')
+  curparams = dataset_params.mining_params;
 
-  [tfiles, models_name] = train_all_exemplars(dataset_params, models, ...
-                                              train_set);  
+  cur_set = get_pascal_set(dataset_params, ...
+                           curparams.set_name,...
+                           curparams.set_name2);
+  
+  if isfield(curparams,'set_maxk')
+    cur_set = cur_set(1:min(length(cur_set), ...
+                            curparams.set_maxk));
+  end
+    
+  [tfiles, models_name] = train_all_exemplars(dataset_params, ...
+                                              models, cur_set);  
   %Load the trained exemplars
   CACHE_FILE = 1;
   STRIP_FILE = 1;
   models = load_all_models(dataset_params, cls, models_name, ...
                            tfiles, CACHE_FILE, STRIP_FILE);
   
+else
+  fprintf(1,['Skipping training becuase dataset_params.mining_params not' ...
+             ' present\n']);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -58,27 +54,30 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Apply trained exemplars on validation set
-if strcmp(dataset_params.model_type,'exemplar') && ...
-      (dataset_params.SKIP_VAL == 0)
+if isfield(dataset_params,'val_params')
+  curparams = dataset_params.val_params;
+  cur_set = get_pascal_set(dataset_params, ...
+                           curparams.set_name,...
+                           curparams.set_name2);
+  if isfield(curparams,'set_maxk')
+    cur_set = cur_set(1:min(length(cur_set), ...
+                            curparams.set_maxk));
+  end
 
-  val_set = get_pascal_set(dataset_params, ...
-                           dataset_params.valset_name,...
-                           dataset_params.valset_name2);
-  val_set = val_set(1:min(length(val_set), dataset_params.valset_maxk));
-  
-  %Apply on validation set
-  dataset_params.params = dataset_params.val_params;
+  dataset_params.params = curparams;;
   dataset_params.params.gt_function = @get_pascal_anno_function;
-  val_files = apply_all_exemplars(dataset_params, models, val_set, ...
-                                  dataset_params.valset_name, []);
+  val_files = apply_all_exemplars(dataset_params, models, cur_set, ...
+                                  curparams.set_name);
 
   %Load validation results
   val_grid = load_result_grid(dataset_params, models, ...
-                              dataset_params.valset_name, val_files);
+                          curparams.set_name, val_files);
   
   %% Perform l.a.b.o.o. calibration and M-matrix estimation
   M = calibrate_and_estimate_M(dataset_params, models, val_grid);
 else
+  fprintf(1,['Skipping validation becuase dataset_params.val_params not' ...
+             ' present\n']);
   M = [];
 end
 
@@ -86,38 +85,52 @@ end
 %%%%%% EXEMPLAR TESTING %%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-test_set = get_pascal_set(dataset_params, ...
-                          dataset_params.testset_name,...
-                          dataset_params.testset_name2);
-test_set = test_set(1:min(length(test_set), dataset_params.testset_maxk));
+%% Apply trained exemplars on test set
+if isfield(dataset_params,'test_params')
+  curparams = dataset_params.test_params;
+  cur_set = get_pascal_set(dataset_params, ...
+                           curparams.set_name,...
+                           curparams.set_name2);
 
-if length(test_set) == 0
-  fprintf(1,'Warning, testset is empty\n');
+  if isfield(curparams,'set_maxk')
+    cur_set = cur_set(1:min(length(cur_set), ...
+                            curparams.set_maxk));
+  end
+  
+  if length(cur_set) == 0
+    fprintf(1,'Warning, testset is empty\n');
+    return;
+  end
+
+  %Apply on validation set
+  dataset_params.params = curparams;
+  dataset_params.params.gt_function = [];
+  test_files = apply_all_exemplars(dataset_params, models, cur_set, ...
+                                  curparams.set_name);
+
+  %Load validation results
+  test_grid = load_result_grid(dataset_params, models, ...
+                               curparams.set_name, test_files);
+  
+else
+  fprintf(1,['Skipping testing becuase dataset_params.test_params not' ...
+             ' present\n']);
+  
+  %If testing is not performed, there is nothing left to do
   return;
 end
-
-%Apply trained exemplars on test set
-dataset_params.params = dataset_params.test_params;
-dataset_params.params.gt_function = [];
-test_files = apply_all_exemplars(dataset_params, models, test_set, ...
-                                 dataset_params.testset_name,[]);
-
-%Load testset results
-test_grid = load_result_grid(dataset_params, models, ...
-                             dataset_params.testset_name, test_files);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%% EXEMPLAR EVALUATION %%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
 %% Evaluation uncalibrated SVM classifiers
 teststruct = pool_results(dataset_params, models, test_grid);
-if strcmp(dataset_params.model_type,'exemplar') && ...
-      (dataset_params.SKIP_EVAL == 0)
+if (dataset_params.SKIP_EVAL == 0)
   [results] = evaluate_pascal_voc_grid(dataset_params, ...
                                        models, test_grid, ...
-                                       dataset_params.testset_name, teststruct);
+                                       curparams.set_name, ...
+                                       teststruct);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -125,7 +138,7 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 show_memex_browser(dataset_params, models, test_grid,...
-                   test_set, dataset_params.testset_name);
+                   cur_set, curparams.set_name);
 
 %%% Show top detections from uncalibrated SVM classifiers
 % show_top_dets(dataset_params, models, test_grid,...
@@ -138,7 +151,8 @@ if length(M) > 0 && (dataset_params.SKIP_EVAL == 0)
   teststruct = pool_results(dataset_params, models, test_grid, M2);
   [results] = evaluate_pascal_voc_grid(dataset_params, ...
                                        models, test_grid, ...
-                                       dataset_params.testset_name, teststruct);
+                                       curparams.set_name,...
+                                       teststruct);
   
   %% Show top detections from l.a.b.o.o.
   show_top_dets(dataset_params, models, test_grid,...
@@ -148,16 +162,15 @@ if length(M) > 0 && (dataset_params.SKIP_EVAL == 0)
   %% Evaluation of laboo + M matrix
   teststruct = pool_results(dataset_params, models, test_grid, M);
   
-  if strcmp(dataset_params.model_type,'exemplar') && ...
-        (dataset_params.SKIP_EVAL == 0)
+  if (dataset_params.SKIP_EVAL == 0)
     [results] = evaluate_pascal_voc_grid(dataset_params, ...
                                          models, test_grid, ...
-                                         dataset_params.testset_name, ...
+                                         curparams.set_name, ...
                                          teststruct);
   end
   
   %% Show top detections for laboo + M matrix
   show_top_dets(dataset_params, models, test_grid,...
-                test_set, dataset_params.testset_name, ...
+                test_set, curparams.set_name, ...
                 teststruct);
 end
