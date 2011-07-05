b5117566fca9bf692b737eab3abad6937de064f1
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-07-05 14:55:10 -0400

    update default uses

--- scripts/get_voc_dataset.m
+++ scripts/get_voc_dataset.m
@@ -1,17 +1,27 @@
-function dataset_params = get_voc_dataset(VOCYEAR,suffix)
+function dataset_params = get_voc_dataset(VOCYEAR, result_dir, datadir)
 %Get the dataset structure for a VOC dataset, given the VOCYEAR
 %string which is something like: VOC2007, VOC2010, ...
 
-if ~exist('suffix','var')
-  suffix = '/nfs/baikal/tmalisie/summer11/';
-  fprintf(1,'Using default dataset directory: %s\n',suffix);
+% Issue warning if you are not me (tmalisie), but want default parameters
+if nargin ~= 3
+  [tmp,user] = unix('whoami');
+  user = strtrim(user);
+  if strcmp(user,'tmalisie') == 0
+    fprintf(1,'Warning user=%s, but defaults only defined for user=%s\n',...
+            user,'tmalisie');
+  end
 end
 
-%%%% SETUP DATASET
-%CHOOSE HOW MANY IMAGES WE APPLY PER CHUNK
+if ~exist('result_dir','var')
+  result_dir = '/nfs/baikal/tmalisie/summer11/';
+  fprintf(1,'Using default dataset directory: %s\n', result_dir);
+end
+
+% Choose the number of images to process in each chunk (for detections)
 dataset_params.NIMS_PER_CHUNK = 4;
 
-dataset_params.devkitroot = [suffix '/' VOCYEAR];
+% Create a root directory
+dataset_params.devkitroot = [result_dir '/' VOCYEAR];
 
 % change this path to a writable local directory for the example code
 dataset_params.localdir = [dataset_params.devkitroot '/local/'];
@@ -20,15 +30,21 @@ dataset_params.localdir = [dataset_params.devkitroot '/local/'];
 dataset_params.resdir = [dataset_params.devkitroot ['/' ...
                     'results/']];
 
-%This is the directory where we dump visualizations into
-[v,r] = unix('hostname');
-if strfind(r,'airbone')==1
-  dataset_params.datadir = '/projects/Pascal_VOC/';
+%This is location of the installed VOC datasets
+if ~exist('datadir','var')
+  [v,r] = unix('hostname');
+  if strfind(r,'airbone')==1
+    dataset_params.datadir = '/projects/Pascal_VOC/';
+  else
+    dataset_params.datadir = '/nfs/hn38/users/sdivvala/Datasets/Pascal_VOC/';
+  end
 else
-  dataset_params.datadir = '/nfs/hn38/users/sdivvala/Datasets/Pascal_VOC/';
+  dataset_aprams.datadir = datadir;
 end
 
 %Some VOC-specific dataset stats
 dataset_params.dataset = VOCYEAR;
 dataset_params.testset = 'test';
+
+%Fill in the params structure with VOC-specific stuff
 dataset_params = VOCinit(dataset_params);
