d4198aa2e5c05a9a4027133ea30938141f22a43b
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-07-15 06:10:07 -0400

    b subtraction is enabled even though b is zero

--- iccv11/apply_boost_M.m
+++ iccv11/apply_boost_M.m
@@ -13,7 +13,7 @@ r = zeros(1,size(boxes,1));
 for i = 1:size(boxes,1)
   % if isfield(M,'C1')
   %   r(i) = (x(:,i)'*M.C1*x(:,i));
-  r(i) = (M.w{exids(i)}'*x(:,i) + sum(x(:,i)));%-M.b{exids(i)};
+  r(i) = (M.w{exids(i)}'*x(:,i) + sum(x(:,i)))-M.b{exids(i)};
   %r(i) = (M.w{exids(i)}'*x(:,i))-M.b{exids(i)};
   %   r(i) = x(:,i)'*M.C*x(:,i) + x(exids(i),i);
   %  end
