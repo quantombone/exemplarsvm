0e7a1ac8a6909a3c624468dd923441ac93bf7e3b
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-08-10 15:01:50 -0400

    apply to corners too

--- util/apply_xform.m
+++ util/apply_xform.m
@@ -9,32 +9,40 @@ if size(c,1)==0
   return;
 end
 
-%if size(c,1) > 1
-  
-  xs = c(:,1:2)';
-  xs(3,:) = 1;
-  d1 = xform*xs;
-  d1 = d1(1:2,:)';
-  
-  xs = c(:,3:4)';
-  xs(3,:) = 1;
-  d2 = xform*xs;
-  d2 = d2(1:2,:)';
-  result = [d1 d2];
-  return;
-%end
 
-for i = 1:size(c,1)
-  curc = c(i,:);
-
-  xs(1:2,1) = curc([1 2])';
-  xs(1:2,2) = curc([3 4])';
-  xs(3,:) = 1;
-
-  d = xform*xs;
-  
-  result(i,1) = d(1,1);
-  result(i,2) = d(2,1);
-  result(i,3) = d(1,2);
-  result(i,4) = d(2,2);  
+CLIP = 0;
+if size(c,2) == 2
+  CLIP = 1;
+  c(:,[3 4]) = 0;
 end
+  
+xs = c(:,1:2)';
+xs(3,:) = 1;
+d1 = xform*xs;
+d1 = d1(1:2,:)';
+
+xs = c(:,3:4)';
+xs(3,:) = 1;
+d2 = xform*xs;
+d2 = d2(1:2,:)';
+result = [d1 d2];
+
+if CLIP == 1
+  result = result(:,1:2);
+end
+
+
+% for i = 1:size(c,1)
+%   curc = c(i,:);
+
+%   xs(1:2,1) = curc([1 2])';
+%   xs(1:2,2) = curc([3 4])';
+%   xs(3,:) = 1;
+
+%   d = xform*xs;
+  
+%   result(i,1) = d(1,1);
+%   result(i,2) = d(2,1);
+%   result(i,3) = d(1,2);
+%   result(i,4) = d(2,2);  
+% end
