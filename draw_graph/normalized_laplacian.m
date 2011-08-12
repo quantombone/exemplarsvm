780ba88d40e9c0e404ff6965154a159d687cb8d5
Tomasz Malisiewicz <malist@gmail.com>
2011-08-12 13:25:27 -0400

    remove graph drawing from here

--- draw_graph/normalized_laplacian.m
+++
@@ -1,9 +0,0 @@
-function L = normalized_laplacian(A)
-A = A - diag(diag(A));
-degs = sum(A,1);
-Tih = spdiag(degs.^-.5);
-L = speye(size(A)) - Tih*A*Tih;
-
-%% enforce machine precision un-symmetry
-%% helps eigs make sure we don't get negative eigenvalues
-L = (L + L')/2;
--- draw_graph/spdiag.m
+++
@@ -1,8 +0,0 @@
-function W=spdiag(D);
-% Timothee Cour, 04-Aug-2008 20:46:38 -- DO NOT DISTRIBUTE
-
-D=D(:);
-n=length(D);
-W = spdiags(D,0,n,n);
-
-
