fffe64a2fddbc0b3b4c251567a99c9c339389261
Tomasz Malisiewicz <tomasz@cmu.edu>
2011-08-23 18:25:03 -0400

    change default directory

--- scripts/load_data_directory.m
+++ scripts/load_data_directory.m
@@ -1,3 +1,5 @@
 function suffix = load_data_directory;
-suffix = '/nfs/baikal/tmalisie/iccv11/';
-suffix = '/nfs/baikal/tmalisie/nn311/';
+%suffix = '/nfs/baikal/tmalisie/iccv11/';
+%suffix = '/nfs/baikal/tmalisie/nn311/';
+%suffix = '/nfs/baikal/tmalisie/small311/';
+suffix = '/lustre/tmalisie/small311/';
