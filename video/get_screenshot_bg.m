40f40592ccdc16dd33b5338f30e4966e6e3303d4
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2011-10-11 17:43:16 -0400

    load default sizes

--- video/get_screenshot_bg.m
+++ video/get_screenshot_bg.m
@@ -1,6 +1,10 @@
 function bg = get_screenshot_bg(NFRAMES,func)
 bg = cell(NFRAMES,1);
-subber=initialize_screenshot;
+
+load subber.mat
+%fprintf(1,'Getting init screenshot in 4 sec\n');
+%pause(4)
+%subber=initialize_screenshot;
 
 if exist('func','var')
   for i = 1:length(bg)
