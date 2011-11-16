b2b1cbf3aea37980d1bc300d4e1f1c2652ff798e
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2011-11-16 17:58:30 -0500

    update files for ease of use

--- demos/voc_demo_esvm.m
+++ demos/voc_demo_esvm.m
@@ -1,9 +1,10 @@
-%clear;
+%This is the main Exemplar-SVM driver program, see README.md for
+%instructions
+%Tomasz Malisiewicz (tomasz@cmu.edu)
 
 %% Initialize dataset
 VOCYEAR = 'VOC2007';
 suffix = load_data_directory;
-suffix = '/nfs/baikal/tmalisie/nn311/';
 dataset_params = get_voc_dataset(VOCYEAR,suffix);
 dataset_params.display = 0;
 
@@ -14,28 +15,28 @@ dataset_params.SKIP_EVAL = 0;
 %% SET SOURCETRAIN/VAL/TEST PARAMS %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-if 1
-  %Initialize framing function
-  init_params.sbin = 8;
-  init_params.goal_ncells = 100;
-  init_params.MAXDIM = 12;
-  init_params.init_function = @initialize_goalsize_model;
-  init_params.init_string = 'g';
-  init_params.init_type = sprintf('%s-%d-%d',...
-                                  init_params.init_string,...
-                                  init_params.goal_ncells,...
-                                  init_params.MAXDIM);
-else
-  %Initialize exemplar framing function
-  init_params.sbin = 8;
-  init_params.hg_size = [8 8];
-  init_params.init_function = @initialize_fixedframe_model;
-  init_params.init_string = 'f';
-  init_params.init_type = sprintf('%s-%d-%d', ...
-                                  init_params.init_string, ...
-                                  init_params.hg_size(1), ...
-                                  init_params.hg_size(2));
-end
+
+%Initialize framing function
+init_params.sbin = 8;
+init_params.goal_ncells = 100;
+init_params.MAXDIM = 12;
+init_params.init_function = @initialize_goalsize_model;
+init_params.init_string = 'g';
+init_params.init_type = sprintf('%s-%d-%d',...
+                                init_params.init_string,...
+                                init_params.goal_ncells,...
+                                init_params.MAXDIM);
+
+% %Initialize exemplar framing function
+% init_params.sbin = 8;
+% init_params.hg_size = [8 8];
+% init_params.init_function = @initialize_fixedframe_model;
+% init_params.init_string = 'f';
+% init_params.init_type = sprintf('%s-%d-%d', ...
+%                                 init_params.init_string, ...
+%                                 init_params.hg_size(1), ...
+%                                 init_params.hg_size(2));
+
 dataset_params.init_params = init_params;
 
 %Initialize exemplar stream
@@ -48,36 +49,30 @@ dataset_params.model_type = 'exemplar';
 
 %Create mining/validation/testing params as defaults
 dataset_params.params = get_default_mining_params;
-%dataset_params.params.nnmode = 'normalizedhog';
 
-if 1
-  %Choose the training function (do_svm, do_rank, ...)
-  %Disable NMS in training params
-  dataset_params.mining_params = dataset_params.params;
-  dataset_params.mining_params.training_function = @do_svm;
-  dataset_params.mining_params.NMS_OS = 1.0;
-  dataset_params.mining_params.MAXSCALE = 0.5;
-  dataset_params.mining_params.TOPK = 100;
-  dataset_params.mining_params.set_name = 'train';
-  %optional cap
-  %dataset_params.mining_params.set_maxk = 0;
-end
 
-if 1
-  dataset_params.val_params = dataset_params.params;
-  dataset_params.val_params.NMS_OS = 0.5;
-  dataset_params.val_params.set_name = 'trainval';
-  %optional cap
-  %dataset_params.val_params.set_maxk = 10000;
-end
+%Choose the training function (do_svm, do_rank, ...)
+%Disable NMS in training params
+dataset_params.mining_params = dataset_params.params;
+dataset_params.mining_params.training_function = @do_svm;
+dataset_params.mining_params.NMS_OS = 1.0;
+dataset_params.mining_params.MAXSCALE = 0.5;
+dataset_params.mining_params.TOPK = 100;
+dataset_params.mining_params.set_name = 'train';
+%optional cap
+%dataset_params.mining_params.set_maxk = 0;
 
-if 1
-  dataset_params.test_params = dataset_params.params;
-  dataset_params.test_params.NMS_OS = 0.5;
-  dataset_params.test_params.set_name = 'test';
-  %optional cap
-  %dataset_params.test_params.set_maxk = 0;
-end
+dataset_params.val_params = dataset_params.params;
+dataset_params.val_params.NMS_OS = 0.5;
+dataset_params.val_params.set_name = 'trainval';
+%optional cap
+%dataset_params.val_params.set_maxk = 10000;
+
+dataset_params.test_params = dataset_params.params;
+dataset_params.test_params.NMS_OS = 0.5;
+dataset_params.test_params.set_name = 'test';
+%optional cap
+%dataset_params.test_params.set_maxk = 0;
 
 %Choose a short string to indicate the type of training run we are doing
 dataset_params.models_name = ...
@@ -86,68 +81,18 @@ dataset_params.models_name = ...
      '.' ...
      dataset_params.model_type];
 
-%classes = {'bus'};
-classes = {...
-    %'aeroplane'
-    %'bicycle'
-    'bird'
-    'boat'
-    'bottle'
-    %'bus'
-    %'cat'
-    'car'
-    %'chair'
-    %'cow'
-    'diningtable'
-    %'dog'
-    %'horse'
-    'motorbike'
-    %'person'
-    %'pottedplant'
-    %'sheep'
-    'sofa'
-    'train'
-    %'tvmonitor'
-};
-classes = dataset_params.classes;
-classes = setdiff(classes,{'person'});
+classes = {'bus'};
 
-%classes = {'tvmonitor'};
-%classes = {'chair'};
-%classes = {'diningtable'};
-%classes = {'bicycle'};
-%classes = {'person'};
-%classes = {'train'};
-%classes = {'bicycle'};
-%classes = {'motorbike'};
-%classes = {'sheep'};
-%classes = {'person'};
-%classes = {'bus'};
+%choose all classes except person
+%classes = dataset_params.classes;
+%classes = setdiff(classes,{'person'});
 
-%classes = {'motorbike'};
-%-2mzo.classes = {'cow'};
-%classes = {'sheep'};
-classes = {'motorbike'};
-classes = {'person'};
-
+%randomize ordering of classes
 myRandomize;
 r = randperm(length(classes));
 classes = classes(r);
-
-classes = {'diningtable','motorbike','bus','train','sheep','cow'};
-classes = sort(classes);
-%myRandomize;
-%r = randperm(length(classes));
-%classes = classes(r);
-
 dataset_params.classes = classes; 
 
-plot_voc_results3(dataset_params);
-return;
-
-%plot_voc_results_horiz(dataset_params);
-%
-%return;
 save_dataset_params = dataset_params;
 for i = 1:length(classes)
   dataset_params = save_dataset_params;
@@ -172,7 +117,7 @@ for i = 1:length(classes)
 
   %dataset_params.JUST_TRAIN = 1;
   %dataset_params.JUST_TRAIN_AND_LOAD = 1;
-  dataset_params.SKIP_M = 1;
-  %NNN = 20;
+  %dataset_params.SKIP_M = 1;
+
   voc_template(dataset_params, classes{i});
 end
