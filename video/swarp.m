bfcb3c0b3641266908fcb508a74b1c132e052afa
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2011-11-18 04:25:42 -0500

    Fix some parameters in the s-warper

--- video/swarp.m
+++ video/swarp.m
@@ -6,37 +6,34 @@ function swarp(Is,bbs)
 
 filestring = 'combo%05d.png';
 
-if ~exist('Is','var')
-  %% generate fake data
-  K = 9;
-  for i = 1:K
-    Is{i} = rand(400,400,3);
-    b = ceil(rand(1,2)*100);
-    rf = max(50,round(100*(randn(1,1)+1)));
-    b = [b b+rf];
-    bbs(i,:) = b;
-    Is{i}(b(2):b(4),b(1):b(3),1:2)=0;
-  end
-end
+% if ~exist('Is','var')
+%   %% generate fake data
+%   K = 9;
+%   for i = 1:K
+%     Is{i} = rand(400,400,3);
+%     b = ceil(rand(1,2)*100);
+%     rf = max(50,round(100*(randn(1,1)+1)));
+%     b = [b b+rf];
+%     bbs(i,:) = b;
+%     Is{i}(b(2):b(4),b(1):b(3),1:2)=0;
+%   end
+% end
+
+Is = cellfun2(@(x)convert_to_I(x),Is);
+
+b = mean(bbs,1);
+W = b(3)-b(1)+1;
+H = b(4)-b(2)+1;
+factor = max(W,H)/100;
+W = W*factor;
+H = H*factor;
 
 K = length(Is);
-KKK = ceil(sqrt(K));
-figure(1)
-clf
-for i = 1:K
-  subplot(KKK,KKK,i)
-  imagesc(Is{i})
-  plot_bbox(bbs(i,:));
-  imbbs(i,:) = [1 1 size(Is{i},2) size(Is{i},1)];
-  plot_bbox(imbbs(i,:),'',[0 0 0])
-  axis image
-  axis off
-end
 
-
-b = [400 400 600 600];
+b = [400 400 400+W 400+H];
 for i = 1:K
   xform = find_xform(bbs(i,:), b);
+  imbbs(i,:) = [1 1 size(Is{i},2) size(Is{i},1)];
   result(i,:) = apply_xform(imbbs(i,:), xform);
 end
 
@@ -61,47 +58,28 @@ for i = 1:size(result,1)
   %find xform from center box to original box
   xform = find_xform(b,bbs(i,:));
   newpoints = apply_xform(points,xform);
-  
-%   xform = find_xform(bbs(i,:), b);
-%   result(i,:) = apply_xform(imbbs(i,:), xform);
-%   d = result(i,:);
-%   d([1 2]) = ceil(d([1 2]));
-%   d([3 4]) = floor(d([3 4]));
-
-%   xform = find_xform(b,bbs(i,:));
-% ;
-  
-  
+    
   clear zi
   for q = 1:3
     zi(:,q) = interp2(Is{i}(:,:,q),newpoints(:,1),newpoints(:,2));
   end
 
-  zi(isnan(zi))=0;
+  zi(isnan(zi))=1;
 
   zi = reshape(zi,size(totalI,1),size(totalI,2),3);
   older = totalI;
   totalI = totalI + zi;
 
-  %figure(2)
-
-  %imshow(zi)
-  %drawnow
-
-  %keyboard
-  imwrite(zi,sprintf(filestring,i));
+  %imwrite(zi,sprintf(filestring,i));
 end
 
 totalI = totalI / size(result,1);
-
-
-%totalI = max(0.0,min(1.0,totalI));
 figure(2)
 clf
 
 imagesc(totalI)
 plot_bbox(b,'center');
-plot_bbox(result,'',[0 0 0]);
+%plot_bbox(result,'',[0 0 0]);
 axis image
 axis off
 drawnow
