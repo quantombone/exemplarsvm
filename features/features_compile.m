980534ddd24253f6e1036b9f48a651c04dd606cf
Tomasz Malisiewicz <malist@gmail.com>
2012-01-06 03:18:40 -0500

    try cxx flags for fconvblas

--- features/features_compile.m
+++ features/features_compile.m
@@ -11,5 +11,8 @@ mex -O features_raw.cc
 
 % mulththreaded convolution without blas (see voc-release-4)
 %mex -O fconvblas.cc -lmwblas -o fconvblas
-mex CC=gcc LD=gcc COPTIMFLAGS='-O3 -DNDEBUG' fconvblas.cc -lmwblas ...
-          -o fconvblas
+%mex CC=gcc LD=gcc COPTIMFLAGS='-O3 -DNDEBUG' fconvblas.cc -lmwblas ...
+%          -o fconvblas
+
+mex CXX=gcc CXXOPTIMFLAGS='-O3 -DNDEBUG -funroll-all-loops' fconvblas.cc ...
+        -lmwblas
