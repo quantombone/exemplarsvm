42b26da1d56540572a8f0b80d750004aec486cd9
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-01-18 18:07:27 -0500

    update comment in function

--- util/get_file_list.m
+++ util/get_file_list.m
@@ -1,4 +1,10 @@
 function files = get_file_list(dirpath)
+% Given a directory, list all files inside the directory and create
+% a cell array of strings, where each string is the file location
+% NOTE: this can be used with convert_to_I, as follows
+% >> files = get_file_list('~/myimages/');
+% >> imagesc(convert_to_I(files{10}))
+
 if ~isstr(dirpath)
   fprintf(1,'get_file_list: Warning, not a directory\n');
   files = {};
