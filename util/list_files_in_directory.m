da2939da38673d14f5e7a986f8893bb91da77057
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-01-25 21:03:42 -0500

    fix bug with providine a regex

--- util/list_files_in_directory.m
+++ util/list_files_in_directory.m
@@ -2,17 +2,23 @@ function files = list_files_in_directory(dirpath)
 % Given a directory, list all files inside the directory and create
 % a cell array of strings, where each string is the file location
 % NOTE: this can be used with convert_to_I, as follows
-% >> files = list_files_in_directory('~/myimages/');
-% >> imagesc(convert_to_I(files{10}))
+% Examples:
+%  >> files = list_files_in_directory('~/myimages/');
+%  >> imagesc(convert_to_I(files{10}))
+%
+% In the case where there are non-image files in the directory
+%  >> files = list_files_in_directory('~/myimages/*jpg');
+%  >> imagesc(convert_to_I(files{10}))
 
 if ~isstr(dirpath)
   fprintf(1,'list_files_in_directory: Warning, not a directory\n');
   files = {};
   return;
 end
+[basedir, other, other2] = fileparts(dirpath);
 
 files = dir(dirpath);
 isdirs = arrayfun(@(x)x.isdir,files);
 files = files(~isdirs);
 files = {files.name};
-files = cellfun2(@(x)[dirpath '/' x],files);
+files = cellfun2(@(x)[basedir '/' x],files);
