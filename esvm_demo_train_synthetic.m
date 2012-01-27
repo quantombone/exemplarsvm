20801b5a01ddab61c75b5fb0585a3c5577b577ee
Tomasz Malisiewicz <malist@gmail.com>
2012-01-27 00:55:23 -0500

    add longer comments

--- esvm_demo_train_synthetic.m
+++ esvm_demo_train_synthetic.m
@@ -8,11 +8,22 @@
 % This file is part of the Exemplar-SVM library and is made
 % available under the terms of the MIT license (see COPYING file).
 % Project homepage: https://github.com/quantombone/exemplarsvm
+%
+% In this demo, I create a random dataset of circular patterns on a
+% random background of noise with extra noise sprinkled on top.
+% Because the circles are synthetically generated, we have access
+% to ground-truth locations of those circles and these are used to
+% define the positive bounding boxes.  The learned Exemplar-SVMs
+% plus the calibration M-matrix are first learned, then applied to
+% a testing set of images along with the top detections.
 function [models,M] = esvm_demo_train_synthetic
 
 addpath(genpath(pwd))
 
 %% Create a synthetic dataset of circles on a random background
+% The resulting images are also corrupted with noise to provide a
+% more difficult case.  Negative images are random noise images
+% without any circular pattern.
 Npos = 100;
 Nneg = 50;
 [pos_set,neg_set] = esvm_generate_dataset(Npos,Nneg);
@@ -51,6 +62,11 @@ val_neg_set = neg_set((Nneg/2+1):end);
 neg_set = neg_set(1:((Nneg/2)));
 
 %% Initialize Exemplars
+% Each exemplar will have a figure, where on the first image is
+% the exemplar's image, along with the exemplar bounding box and
+% HOG grid overlayed.  The second image shows the HOG mask along
+% with its offset to the ground-truth bounding box.  The third
+% image shows the initial HOG features used to define the exemplar.
 initial_models = esvm_initialize_exemplars(e_stream_set, params, ...
                                            models_name);
 
@@ -62,6 +78,14 @@ train_params.detect_exemplar_nms_os_threshold = 1.0;
 train_params.detect_max_windows_per_exemplar = 100;
 
 %% Perform Exemplar-SVM training
+% Because display is turned on, we will show the result of each
+% exemplar's training iteration.   Each iteration shows a
+% diagnostic first column then the remaining rows are the top
+% negative support vectors used to define the exemplar's decision
+% boundary.  The diagnostic row shows: exemplar, w's positive
+% part, w's negative part, and four mean support vector images,
+% where the means are computed with the first 1:N/4, 1:N/2, .. ,
+% 1:N support vectors.
 [models] = esvm_train_exemplars(initial_models, ...
                                 neg_set, train_params);
 
@@ -76,6 +100,8 @@ val_set_name = 'valset';
 val_grid = esvm_detect_imageset(val_set, models, val_params, val_set_name);
 
 %% Perform Platt calibration and M-matrix estimation
+% The display will include the fit sigmoid, positive exemplar, as
+% well as the top detections.
 M = esvm_perform_calibration(val_grid, val_set, models, val_params);
 
 %% Define test-set
@@ -92,6 +118,10 @@ test_grid = esvm_detect_imageset(test_set, models, test_params, test_set_name);
 test_struct = esvm_pool_exemplar_dets(test_grid, models, M, test_params);
 
 %% Show top detections
+% Each resulting figure will show the source exemplar/weights (left
+% column)from the top detection as well as the detection box in the
+% resulting image (top right) and the exemplar inpainting (bottom
+% right).
 maxk = 20;
 allbbs = esvm_show_top_dets(test_struct, test_grid, test_set, models, ...
                        params, maxk, test_set_name);
