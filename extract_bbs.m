295c628577dee13b81f54c0ec1e17f562fc0e78c
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-03 15:59:18 -0500

    visualize files

---
+++ extract_bbs.m
@@ -0,0 +1,15 @@
+function bbs = extract_bbs(data_set)
+
+bbs = cell(0,1);
+for i = 1:length(data_set)
+  if isfield(data_set{i},'objects')
+    for j = 1:length(data_set{i}.objects)
+      bb = data_set{i}.objects(j).bbox;
+      bb(12) = 0;
+      bb(11) = i;
+      bbs{end+1} = bb;
+    end
+  end  
+end
+
+bbs = cat(1,bbs{:});
---
+++ vis_box_tiles.m
@@ -0,0 +1,61 @@
+function [III,locations] = vis_box_tiles(Is,L)
+%Given a bunch of images (all of same size) and a bb per image
+%Create a visualization
+
+if nargin == 1
+  L = extract_bbs(Is);
+end
+
+uL = unique(L(:,11));
+
+Is = Is(uL);
+
+[tmp,ids] = ismember(L(:,11),uL);
+L(:,11) = ids;
+
+Is = cellfun2(@(x)toI(x),Is);
+start_length = length(Is);
+S = ceil(sqrt(length(Is)));
+fillmore = S*S-length(Is);
+
+Is = cat(1,Is,repmat({Is{1}*0+0},fillmore,1));
+PAD = 10;
+Is = cellfun2(@(x)pad_image(x,PAD,[1 1 1]),Is);
+
+Is = reshape(Is,[S S])';
+for i = 1:S
+  Irow{i} = cat(2,Is{i,:});
+end
+
+III = cat(1,Irow{:});
+
+
+counter = 1;
+locations = zeros(start_length,size(L,2));
+
+for i = 1:S
+  for j = 1:S
+    if (counter > start_length)
+      break;
+    end
+    cur = L(counter,:);
+    cur([1 3]) = cur([1 3])+(j-1)*size(Is{1},2);
+    cur([2 4]) = cur([2 4])+(i-1)*size(Is{1},1);
+    cur = cur + PAD;
+    
+    locations(counter,:) = cur;
+    counter = counter + 1;
+
+  end
+end
+
+
+if nargout == 0
+  imagesc(III)
+  for i = 1:size(locations,1)
+    plot_bbox(locations(i,:),num2str(locations(i,end)),[1 0 0],[1 0 ...
+                    0])
+  end
+end
+
+%L(:,end)
