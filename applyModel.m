da3120f628a0d1249151f87f89bca922a642ab53
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-07 20:24:01 -0500

    Allow for handing of images in block format

--- applyModel.m
+++ applyModel.m
@@ -5,9 +5,23 @@ test_params = model.params;
 test_params.detect_exemplar_nms_os_threshold = 1.0;
 test_params.detect_max_windows_per_exemplar = 500;
 test_params.detect_keep_threshold = -2.0;
+test_params.display_detections = 1;
 
-%% Apply on test set
-test_grid = esvm_detect_imageset(data_set, model, test_params);
+if isnumeric(data_set) && size(data_set,4)==1 && size(data_set,3)== ...
+      3
+  data_set = {data_set};
+elseif isnumeric(data_set) && size(data_set,4) >1 && size(data_set, ...
+                                                  3)==3
+  for i = 1:size(data_set,4)
+    
+    curI = data_set(:,:,:,i);
+    boxes{i} = esvm_detect_imageset({curI},model,test_params);
+  end
+else
+
+  %% Apply on test set
+  test_grid = esvm_detect_imageset(data_set, model, test_params);
+end
 
 %% Apply calibration matrix to test-set results
 boxes = esvm_pool_exemplar_dets(test_grid, model.models, [], ...
