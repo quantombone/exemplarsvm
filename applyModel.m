e9703856edd3a508c42a9a7c0d2c2970d225de8f
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-08 20:33:53 -0500

    function needs fixing

--- applyModel.m
+++ applyModel.m
@@ -1,10 +1,12 @@
 function boxes = applyModel(data_set, model, test_set_name)
 
+data_set = get_screenshot_bg(1000);
+
 %% Define test-set parameters
 test_params = model.params;
 test_params.detect_exemplar_nms_os_threshold = 1.0;
 test_params.detect_max_windows_per_exemplar = 500;
-test_params.detect_keep_threshold = -2.0;
+test_params.detect_keep_threshold = -1.0;
 test_params.display_detections = 1;
 
 if isnumeric(data_set) && size(data_set,4)==1 && size(data_set,3)== ...
@@ -20,10 +22,10 @@ elseif isnumeric(data_set) && size(data_set,4) >1 && size(data_set, ...
 else
 
   %% Apply on test set
-  test_grid = esvm_detect_imageset(data_set, model, test_params);
+  boxes = esvm_detect_imageset(data_set, model, test_params);
 end
 
 %% Apply calibration matrix to test-set results
-boxes = esvm_pool_exemplar_dets(test_grid, model.models, [], ...
-                                      test_params);
+boxes = esvm_pool_exemplar_dets(boxes, model.models, [], ...
+                                test_params);
 
