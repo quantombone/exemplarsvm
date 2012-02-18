b6f4f4a2b14c45b2b63e2dbc462032f569959dc7
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-18 18:28:23 -0500

    learn those dts

--- learnDalalTriggs.m
+++ learnDalalTriggs.m
@@ -1,5 +1,8 @@
 function model = learnDalalTriggs(data_set, cls, params)
-% Learn a DalalTriggs template detector
+% Learn a DalalTriggs template detector, with latent updates,
+% perturbed assignments (which help avoid local minima), and
+% ability to use different features.
+%
 % Copyright (C) 2011-12 by Tomasz Malisiewicz
 % All rights reserved. 
 %
@@ -13,12 +16,12 @@ if ~exist('cls','var')
   error('Needs class as input');
 end
 
-data_directory = '/Users/tomasz/projects/pascal/';
-dataset_directory = 'VOC2007';
-
-if ~exist('data_set','var')
-  load(sprintf('%s/%s/trainval.mat',...
-               data_directory, dataset_directory),'data_set');
+if length(data_set) == 0
+  fprintf(1,'No dataset provided, loading default DATA');
+  
+  data_directory = '/Users/tomasz/projects/pascal/';
+  dataset_directory = 'VOC2007';  
+  data_set = install_dataset(data_directory, dataset_directory);
 end
 
 if ~exist('params','var') || length(params) == 0
@@ -70,7 +73,6 @@ params.latent_os_thresh = 0.7;
 
 
 model = esvm_initialize_dt(data_set, cls, params);
-
 model = esvm_train(model);
 
 for niter = 1:params.latent_iterations
