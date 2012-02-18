4d344c1b95b8147eb677046c38a9f7ff4b057623
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-18 18:27:31 -0500

    apply the models

--- applyModel.m
+++ applyModel.m
@@ -1,11 +1,22 @@
 function boxes = applyModel(data_set, model, test_set_name)
+% Function to apply a model to a data-set
+% Inputs:
+%    data_set: a dataset of images
+%    model: a model to apply to those images
+%    test_set_name: the name of the testset (optional) used to
+%       cache results
+% Outputs:
+%    boxes: a cell array of boxes
+
+if ~exist('test_set_name','var')
+  test_set_name = '';
+end
 
 %% Define test-set parameters
 test_params = model.params;
 test_params.detect_exemplar_nms_os_threshold = 1.0;
 test_params.detect_max_windows_per_exemplar = 500;
 test_params.detect_keep_threshold = -2.0;
-
 
 if length(data_set) == 0
   data_set = get_screenshot_bg(50,@(x)imresize_max(x,400));
@@ -15,21 +26,28 @@ if length(data_set) == 0
   %test_params.detect_max_scale = .1;
 end
 
-
-if isnumeric(data_set) && size(data_set,4)==1 && size(data_set,3)== ...
-      3
+if isnumeric(data_set) && ...
+      size(data_set,4) == 1 && ...
+      size(data_set,3) == 3
   data_set = {data_set};
-elseif isnumeric(data_set) && size(data_set,4) >1 && size(data_set, ...
-                                                  3)==3
+  
+  %Given a single image
+  boxes = esvm_detect_imageset(data_set, model, test_params, test_set_name);
+elseif isnumeric(data_set) && ...
+      size(data_set,4) > 1 && ...
+      size(data_set, 3)==3
+  
+  %Given a tensor of images
   for i = 1:size(data_set,4)
-    
     curI = data_set(:,:,:,i);
     boxes{i} = esvm_detect_imageset({curI},model,test_params);
+    if size(boxes{i},1) > 0
+      boxes{i}(:,11) = i;
+    end
   end
 else
-
   %% Apply on test set
-  boxes = esvm_detect_imageset(data_set, model, test_params);
+  boxes = esvm_detect_imageset(data_set, model, test_params, test_set_name);
 end
 
 %% Apply calibration matrix to test-set results
