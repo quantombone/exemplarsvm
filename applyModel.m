b52f9a40c167d6de240d8050c72fe5dfaa7ba8d1
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-18 18:20:17 -0500

    by default capture screenshots

--- applyModel.m
+++ applyModel.m
@@ -1,15 +1,20 @@
 function boxes = applyModel(data_set, model, test_set_name)
 
-if length(data_set) == 0
-  data_set = get_screenshot_bg(1000,@(x)imresize_max(x,400));
-end
-
 %% Define test-set parameters
 test_params = model.params;
 test_params.detect_exemplar_nms_os_threshold = 1.0;
 test_params.detect_max_windows_per_exemplar = 500;
 test_params.detect_keep_threshold = -2.0;
-%test_params.display_detections = 1;
+
+
+if length(data_set) == 0
+  %data_set = get_screenshot_bg(50,@(x)imresize_max(x,400));
+  data_set = get_screenshot_bg(20);
+  test_params.display_detections = 1;
+  test_params.write_top_detection = 1;
+  test_params.detect_max_scale = .1;
+end
+
 
 if isnumeric(data_set) && size(data_set,4)==1 && size(data_set,3)== ...
       3
