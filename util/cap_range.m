e4f2694e877a45a3ddf01f1ad0feadbc9ada32f0
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-19 18:16:35 -0500

    function works two ways now

--- util/cap_range.m
+++ util/cap_range.m
@@ -1,3 +1,11 @@
-function r = cap_range(r2,starter,ender)
-r = min(r2,ender);
-r = max(r,starter);
+function values_capped = cap_range(values,min_value,max_value)
+% Cap the values to the range [min_value,max_value]
+% [values_capped] = cap_range(values,min_value,max_value);
+% [values_capped] = cap_range(values,[min_value max_value]);
+%
+if ~exist('max_value','var')
+  max_value = min_value(2);
+  min_value = min_value(1);
+end
+values_capped = min(values,max_value);
+values_capped = max(values_capped,min_value);
