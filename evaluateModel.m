be96c33893817f1bfc4921dd9e213b1b2d2a83be
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-19 18:21:47 -0500

    unbreak function

--- evaluateModel.m
+++ evaluateModel.m
@@ -1,5 +1,4 @@
 function results = evaluateModel(data_set, boxes, cls)
 
-
-results = esvm_evaluate_pascal_voc(boxes, data_set, model, params);
+results = esvm_evaluate_pascal_voc(boxes, data_set, cls);%, model, params);
 
