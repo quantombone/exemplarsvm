f6c4b6f03671570f6ebb2e5b581648ec5ffb362e
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-19 18:22:12 -0500

    show nicer models

--- showTopDetections.m
+++ showTopDetections.m
@@ -12,22 +12,37 @@ mh = round(mean(boxes(:,3)-boxes(:,1)));
 
 PSIZE = round((mw+mh)/2*.4);
 
+[aa,bb] = sort(boxes(:,end),'descend');
+boxes = boxes(bb,:);
 
 for i = 1:L
   fprintf(1,'.');
   b = boxes(i,:);
 
-  I = toI(data_set{b(11)});
-  I = pad_image(I,PSIZE);
-  b = round(b + PSIZE);
 
+  I = toI(data_set{b(11)});
+  %I = pad_image(I,PSIZE);
+  %b = round(b + PSIZE);
+  b = round(b);
+
+  us = b(2):b(4);
+  vs = b(1):b(3);
+  
+  goodu = find(us>=1 & us<=size(I,1));
+  goodv = find(vs>=1 & vs<=size(I,2));
+  
+  crops{i} = zeros(length(us),length(vs),3);
+  crops{i}(goodu,goodv,:) = I(us(goodu),vs(goodv),:);
+  
+
+  %crops{i} = I(b(2):b(4),b(1):b(3),:);
   try
-    crops{i} = I(b(2):b(4),b(1):b(3),:);
     crops{i} = imresize(crops{i},round([mw mh]),'bicubic');
-    crops{i} = max(0.0,min(1.0,crops{i}));
   catch
-    crops{i} = zeros(mw,mh,3);
+    keyboard
   end
+  crops{i} = max(0.0,min(1.0,crops{i}));
+
 end
 
 K1 = ceil(sqrt(L));
