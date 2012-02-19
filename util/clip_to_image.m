4811d7172d3caaf62b1960f5cbaeb8a79b2f38cd
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-19 18:16:47 -0500

    clip_to_image had a bug in it

--- util/clip_to_image.m
+++ util/clip_to_image.m
@@ -1,13 +1,15 @@
-function boxes = clip_to_image(boxes,imbb)
-%clip boxes to image (just changes the max dimensions)
+function boxes = clip_to_image(boxes, imbb)
+% Clip boxes to image dimensions
+% boxes = clip_to_image(boxes, imbb)
+% imbb is a 4-vector which is generally [1 1 size(I,2) size(I,1)]
+%
+
 if size(boxes,1) == 0
   return;
 end
 
-for i = 1:2
-  boxes(:,i) = max(imbb(i),boxes(:,i));
-end
+boxes(:,1) = cap_range(boxes(:,1),imbb([1 3]));
+boxes(:,3) = cap_range(boxes(:,3),imbb([1 3]));
 
-for i = 3:4
-  boxes(:,i) = min(imbb(i),boxes(:,i));
-end
+boxes(:,2) = cap_range(boxes(:,2),imbb([2 4]));
+boxes(:,4) = cap_range(boxes(:,4),imbb([2 4]));
