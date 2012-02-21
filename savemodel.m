dfd4ddea143a08d4c0fd8240a5a287efc46ba082
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-21 14:42:45 -0500

    save the model

--- savemodel.m
+++ savemodel.m
@@ -1,3 +1,3 @@
 function savemodel(model,cls)
 
-save(sprintf('/csail/vision-videolabelme/databases/SUN11/dt-models/%s.mat',cls),'model');
+save(sprintf('/csail/vision-videolabelme/databases/SUN11/dt-models/another.%s.mat',cls),'model');
