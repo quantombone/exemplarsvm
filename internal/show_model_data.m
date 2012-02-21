0753929b2d7a683a4623173a77b30cad70d1d112
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-21 14:17:23 -0500

    show model data

--- internal/show_model_data.m
+++ internal/show_model_data.m
@@ -1,17 +1,18 @@
-function show_model_data(model,K)
-%Shows the model's positives and negatives
+function Icur = show_model_data(model, K)
+%Shows the model's positives and negatives as crops in one large matrix
 
 [aa,bb] = sort(model.models{1}.w(:)'*model.models{1}.x,'descend');
 Icur = esvm_show_det_stack(model.models{1}.bb(bb,:),model.data_set, ...
                            K,K,model.models{1});
-
-[aa,bb] = sort(model.models{1}.w(:)'*model.models{1}.svxs,'descend');
-Icur2 = esvm_show_det_stack(model.models{1}.svbbs(bb,:), ...
-                            model.data_set, K,K, ...
-                            model.models{1});
-Ipad = zeros(size(Icur,1),10,3);
-Ipad(:,:,1) = 1;
-Icur = cat(2,Icur,Ipad,Icur2);
+if numel(model.models{1}.svxs) > 0
+  [aa,bb] = sort(model.models{1}.w(:)'*model.models{1}.svxs,'descend');
+  Icur2 = esvm_show_det_stack(model.models{1}.svbbs(bb,:), ...
+                              model.data_set, K,K, ...
+                              model.models{1});
+  Ipad = zeros(size(Icur,1),10,3);
+  Ipad(:,:,1) = 1;
+  Icur = cat(2,Icur,Ipad,Icur2);
+end
 figure(1)
 clf
 imagesc(Icur)
