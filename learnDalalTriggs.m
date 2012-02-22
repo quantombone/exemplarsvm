34d9b5c3dca62204acd3544e153fef5b1a059497
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-22 00:38:38 -0500

    one stage mining

--- learnDalalTriggs.m
+++ learnDalalTriggs.m
@@ -31,33 +31,48 @@ else
   params = esvm_get_default_params(params);
 end
 
-params.display = 0;  
+params.display = 1;  
 params.dump_images = 0;
 params.detect_max_windows_per_exemplar = 200;
 params.train_max_negatives_in_cache = 20000;
-params.train_max_mined_images = 500;
+params.max_number_of_positives = 1000;
+params.train_max_mined_images = 10000;
 params.latent_iterations = 2;
-params.train_svm_c = 1;
-params.train_max_windows_per_iteration = 5000;
+params.train_svm_c = .1;
+params.train_max_windows_per_iteration = 3000;
 % for dalaltriggs, it seams having same constant on positives as
 % negatives is better than using 50
 params.train_positives_constant = 1;
 params.mine_from_negatives = 1;
-params.mine_from_positives = 0;
+params.mine_from_positives = 1;
 params.mine_skip_positive_objects_os = .2;
+params.mine_from_positives_do_latent_update = 1;
 params.train_max_scale = 1.0;
-params.latent_os_thresh = 0.7;
-params.dt_initialize_with_flips = 1;
+params.latent_os_thresh = 0.5;
+params.dt_initialize_with_flips = 0;
 
 %params.detect_pyramid_padding = 0;
 
 starttime = tic;
 model = esvm_initialize_dt(data_set, cls, params);
-model = esvm_train(model);
 
+%params.mine_from_positives = 1;
+%params.mine_from_negatives = 0;
+%model.params = esvm_get_default_params(params);
+model = esvm_train(model);
+if 0
 for niter = 1:params.latent_iterations
-  model = esvm_latent_update_dt(model);
+  %model = esvm_latent_update_dt(model);
+  params.mine_from_positives = 1;
+  params.mine_from_negatives = 0;
+  model.params = esvm_get_default_params(params);
   model = esvm_train(model);
+  
+  params.mine_from_positives = 0;
+  params.mine_from_negatives = 1;
+  model.params = esvm_get_default_params(params);
+  model = esvm_train(model);
+end
 end
 
 model.learn_time = toc(starttime);
