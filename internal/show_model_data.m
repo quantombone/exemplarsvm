bea50c9e04d917a4632b0e668457d62441701b19
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-24 15:48:04 -0500

    show more verbose comments

--- internal/show_model_data.m
+++ internal/show_model_data.m
@@ -1,5 +1,15 @@
 function Icur = show_model_data(model, K)
-%Shows the model's positives and negatives as crops in one large matrix
+%Shows the model's positives and negatives as crops in one large
+%matrix
+%  Inputs:
+%     model: the model
+%     K: the KxK grid which shows positive and negatives
+%  Outputs:
+%    The image which shows results (if specified, doesn't do the showing)
+
+if ~exist('K','var')
+  K = 5;
+end
 
 [aa,bb] = sort(model.models{1}.w(:)'*model.models{1}.x,'descend');
 Icur = esvm_show_det_stack(model.models{1}.bb(bb,:),model.data_set, ...
@@ -13,9 +23,12 @@ if numel(model.models{1}.svxs) > 0
   Ipad(:,:,1) = 1;
   Icur = cat(2,Icur,Ipad,Icur2);
 end
-figure(1)
-clf
+if nargout == 1
+  return;
+end
+%else show
+objective = evaluate_obj(model);
 imagesc(Icur)
-title(sprintf('%s',model.model_name),'FontSize',20);
+title(sprintf('%s: objective=%.3f',model.model_name,objective),'FontSize',20);
 drawnow
 snapnow
