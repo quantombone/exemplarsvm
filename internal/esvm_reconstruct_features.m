04567dec01b665e1750b1d0812307a026274155b
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-25 01:28:53 -0500

    fixed for new model format, does not require feature size hardcoded

--- internal/esvm_reconstruct_features.m
+++ internal/esvm_reconstruct_features.m
@@ -1,4 +1,5 @@
-function [target_id, target_x] = esvm_reconstruct_features(cb, N, set1, set2)
+function [target_id, target_x] = esvm_reconstruct_features(cb, model, ...
+                                                  data_set, N)
 % Extract top N detection feature vectors from bounding boxes and the
 % to be-computed feature pyramid. This allows us to only store
 % (flip,scale,offset) information instead of the 8*8*31 numbers for
@@ -14,45 +15,29 @@ function [target_id, target_x] = esvm_reconstruct_features(cb, N, set1, set2)
 % available under the terms of the MIT license (see COPYING file).
 % Project homepage: https://github.com/quantombone/exemplarsvm
 
-if ~exist('set1','var')
-  set1 = 'trainval';
-  set2 = '';
-end
-if ~exist('set2','var')
-  set2 = '';
-end
-
 if ~exist('N','var')
-  N = 100;
+  N = size(cb,1);
 end
+%[~,bb] = sort(cb(:,end),'descend');
+N = min(N,size(cb,1));
 
-
-%ONLY TAKE TRAINVAL HITS, no need for testing ones quite yet
-trainval = get_pascal_bg(set1,set2);
-ids = cellfun(@(x)get_file_id(x),trainval);
-goods = find(ismember(cb(:,11),ids));
-
-[tmp,bb] = sort(cb(goods,end),'descend');
-bb = goods(bb);
-N = min(N,length(bb));
-
-fprintf(1,'Obtaining top N=%d feature vectors',N);
+fprintf(1,'Obtaining top N=%d feature vectors\n',N);
+hg_size = model.models{1}.hg_size;
 
 if nargout == 2
-  target_x = zeros(8*8*features, N);
+  target_x = zeros(prod(hg_size), N);
 end
 target_id = cell(1, N);
-VOCinit;
 
 starter = tic;
 for i = 1:N
   fprintf(1,'.');
 
-  target_id{i}.scale = cb(bb(i),8);
-  target_id{i}.offset = cb(bb(i),9:10);
-  target_id{i}.flip = cb(bb(i),7);
-  target_id{i}.bb = cb(bb(i),1:4);
-  target_id{i}.curid = cb(bb(i),11);
+  target_id{i}.scale = cb(i,8);
+  target_id{i}.offset = cb(i,9:10);
+  target_id{i}.flip = cb(i,7);
+  target_id{i}.bb = cb(i,1:4);
+  target_id{i}.curid = cb(i,11);
   
   %only do the feature extraction (which takes time!) when two
   %outputs are requested
@@ -60,8 +45,7 @@ for i = 1:N
     continue
   end
   
-  I = convert_to_I(sprintf(VOCopts.imgpath,...
-                           sprintf('%06d',cb(bb(i),11))));
+  I = toI(data_set{cb(i,11)});
 
   if (target_id{i}.flip == 1)
     I = flip_image(I);
@@ -70,12 +54,22 @@ for i = 1:N
   I = resize(I,target_id{i}.scale);
   
   %%NOTE: both sbin=8 and padder=5 are hard-coded here
-  fprintf(1,'Note: hardcoded esvm_features\n');
-  full = esvm_features(I,8);
-  f = padarray(full,[6 6 0]);
+  %fprintf(1,'Note: hardcoded esvm_features\n');
+  
+  full = model.params.init_params.features(I, ...
+                                           model.params ...
+                                           .init_params.sbin);
+  
+  p = model.params.detect_pyramid_padding;
+  f = padarray(full,[p+1 p+1 0]);
 
-  f = f(target_id{i}.offset(1)+6+(0:7)-1,...
-        target_id{i}.offset(2)+6+(0:7)-1,:);
+  %rootmatch = cellfun2(@(x)(x-model.models{1}.b),fconvblas(f, {model.models{1}.w}, 1, 1));  
+  % s = model.models{1}.w(:)'*reshape(f(target_id{i}.offset(1)-1+(1:(hg_size(1))),...
+  %                                     target_id{i}.offset(2)-1+(1:(hg_size(2))),:), ...
+  %                                   [],1)-model.models{1}.b
+   
+  f = f(target_id{i}.offset(1)-1+(1:(hg_size(1))), ...
+        target_id{i}.offset(2)-1+(1:(hg_size(2))),:);
 
   target_x(:,i) = f(:);
 end
