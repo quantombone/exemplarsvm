a59c9f61767cc94f1da31041c3e0c9afbd48e5f3
Tomasz J Malisiewicz <tomasz@vision01.csail.mit.edu>
2012-02-25 19:27:33 -0500

    minor change

--- demo_dt.m
+++ demo_dt.m
@@ -4,26 +4,29 @@
 %data_set = esvm_generate_dataset(10,10,'circle');
 
 %load ~/projects/pascal/VOC2007/trainval.mat 
-
+addpath(genpath(pwd))
 if ~exist('data_set','var')
   load /csail/vision-videolabelme/databases/SUN11/trainval.mat 
 end
+
 
 classes = {'chair','laptop','table',...
            'television','sink','sofa', ...
            'toilet' };
 
-classes = {'door','oven','bottle','bowl','painting','pillow', ...
-           'faucet','refrigerator','microwave','window','faucet','cushion','curtain','cabinet','floor','ceiling','plant','can','column','pot','lamp','sculpture','book','box','bottle','sign','flower','bookshelf','stair','screen','glass','carpet','towel','outlet','stool','cup'};
-
+classes = {'door','bottle','bowl','painting','pillow', ...
+           'faucet','window','faucet','cushion','curtain','cabinet','floor','ceiling','plant','can','column','pot','lamp','sculpture','book','box','bottle','sign','flower','bookshelf','stair','screen','glass','carpet','towel','outlet','stool','cup'};
 % classes = {'sink'};
 % classes = {'laptop'};
 % classes = {'sofa'};
 % classes = {'chair'};
 % classes = {'toilet'};
 % classes = {'table'};
-clear models;
+myRandomize;
+classes = classes(randperm(length(classes)));
+matlabpool open
 parfor i = 1:length(classes)
-  model = learnDalalTriggs(data_set, classes{i});
+  model = learnDalalTriggsSave(data_set, classes{i});
 end
+matlabpool close
 %  savemodel(models{i},classes{i});
