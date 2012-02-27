ed950022a83646f09d41111c83740ea60f82322b
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-02-26 21:38:13 -0500

    save into final models directory

--- savemodel.m
+++ savemodel.m
@@ -1,3 +1,8 @@
 function savemodel(model,cls)
 
-save(sprintf('/csail/vision-videolabelme/databases/SUN11/dt-models/longmine.%s.mat',cls),'model');
+save(sprintf('/csail/vision-videolabelme/databases/SUN11/final-models/longmine-%s.mat',cls),'model');
+%try
+  save(sprintf('~/home_ac_a-5-%s.mat',cls),'model');
+catch
+  fprintf(1,'Could not save model\n');
+end
