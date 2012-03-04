76d9f5062d4cb8ced7002fa8e9d92bdfc7131f41
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-03-04 16:19:49 -0500

    add libsvmpredict compilation

--- libsvm/libsvm_compile.m
+++ libsvm/libsvm_compile.m
@@ -5,6 +5,6 @@ mex -largeArrayDims -O -c svm_model_matlab.c
 mex -largeArrayDims -O libsvmtrain.c svm.o svm_model_matlab.o
 
 %These files are not used
-%mex -O svmpredict.c svm.obj svm_model_matlab.obj
+mex -O libsvmpredict.c svm.o svm_model_matlab.o
 %mex -O libsvmread.c
 %mex -O libsvmwrite.c
