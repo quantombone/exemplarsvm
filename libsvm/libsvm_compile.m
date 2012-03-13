bb3667ecbcf91f3a862f19efa17a3dff19d05cca
Tomasz J Malisiewicz <tomasz@vision01.csail.mit.edu>
2012-03-13 14:24:26 -0400

    update comming flags

--- libsvm/libsvm_compile.m
+++ libsvm/libsvm_compile.m
@@ -1,10 +1,11 @@
 % add -largeArrayDims on 64-bit machines
 
-mex -largeArrayDims -O -c svm.cpp
-mex -largeArrayDims -O -c svm_model_matlab.c
-mex -largeArrayDims -O libsvmtrain.c svm.o svm_model_matlab.o
+mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims -O -c svm.cpp
+mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims -O -c svm_model_matlab.c
+mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims -O libsvmtrain.c svm.o svm_model_matlab.o
 
+mex CFLAGS="\$CFLAGS -std=c99" -O libsvmpredict.c svm.o ...
+    svm_model_matlab.o
 %These files are not used
-mex -O libsvmpredict.c svm.o svm_model_matlab.o
 %mex -O libsvmread.c
 %mex -O libsvmwrite.c
