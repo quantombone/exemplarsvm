e65141f6d5ffc4258c31c7dd1130b61bf9af958c
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-03-14 05:23:31 -0400

    eval code changed interface

--- VOCcode/VOCevaldet.m
+++ VOCcode/VOCevaldet.m
@@ -1,60 +1,17 @@
-function [rec,prec,ap,apold,fp,tp,npos,is_correct] = ...
-    VOCevaldet(test_set,BB,cls,params)
-
+function [result] = VOCevaldet(test_set,BB,cls,params)
+% Evaluates the set of bounding boxes on the dataset
+% called test_set with the PASCAL VOC criterion.
+%
 if ~exist('params','var')
   params.evaluation_minoverlap = 0.5;
   params.display = 1;
 end
 
-% % load test set
-% tic
-% cp=sprintf(VOCopts.annocachepath,VOCopts.testset);
-% if exist(cp,'file')
-%   if ~exist('recs','var')
-%     fprintf('%s: pr: loading ground truth\n',cls);
-    
-%     load(cp,'gtids','recs');
-%     %keyboard
-%   end
-% else
-%     %[gtids,t]=textread(sprintf(VOCopts.imgsetpath,...
-%     %                           VOCopts.testset),'%s %d');
-%     [gtids,t] = textread(sprintf(VOCopts.clsimgsetpath, ...
-%                                    cls,VOCopts.testset),'%s %d');
-    
-    
-%     for i=1:length(gtids)
-%         % display progress
-%         if toc>1
-%             fprintf('%s: pr: load: %d/%d\n',cls,i,length(gtids));
-%             drawnow;
-%             tic;
-%         end
-%         if t(i)~=-1
-%           % read annotation
-%           recs{i}=PASreadrecord(sprintf(VOCopts.annopath, ...
-%                                         gtids{i}));
-%         else
-%           recs{i} = [];
-%         end
-%     end
-%     save(cp,'gtids','recs');
-% end
-
 fprintf('%s: pr: evaluating detections\n',cls);
-
-% if strcmp(VOCopts.dataset,'VOC2007')
-%   for i = 1:length(gtids)
-%     gtids{i} = ['2007_' gtids{i}];
-%   end
-% end
-% hash image ids
-%hash=VOChash_init(gtids);
-        
+       
 % extract ground truth objects
-
 npos=0;
-gt(length(test_set))=struct('BB',[],'diff',[],'det',[]);
+gt(length(test_set))=struct('BB',[],'diff',[],'det',[],'objectid',[],'imageid',[]);
 for i=1:length(test_set)
     % extract objects of class
     if numel(test_set{i})==0
@@ -67,51 +24,35 @@ for i=1:length(test_set)
     else
       clsinds=strmatch(cls,{test_set{i}.objects(:).class},'exact');
       gt(i).BB=cat(1,test_set{i}.objects(clsinds).bbox)';
-      gt(i).diff=[test_set{i}.objects(clsinds).difficult];
+      gt(i).diff=[test_set{i}.objects(clsinds).difficult];% | [test_set{i}.objects(clsinds).occluded];
       gt(i).det=false(length(clsinds),1);
+      gt(i).imageid = gt(i).diff*0+i;
+      gt(i).objectid = 1:length(gt(i).imageid);
       %skip difficult ones in evaluation
       npos=npos+sum(~gt(i).diff);
     end
 
 end
-
-% if isfield(VOCopts,'filename')
-%   filename = VOCopts.filename;
-% else
-%   filename = sprintf(VOCopts.detrespath,id,cls);
-% end
-
-% % % load results
-% [ids,confidence,b1,b2,b3,b4]=textread(filename,'%s %f %f %f %f %f');
-% BB=[b1 b2 b3 b4]';
-
-%BB = stuff{1}.BB;
-%ids = stuff{1}.ids;
-%confidence = stuff{1}.conf;
-
-% if strcmp(VOCopts.dataset,'VOC2007')
-%   for i = 1:length(ids)
-%     ids{i} = ['2007_' ids{i}];
-%   end
-% end
-
+npos
 sss = tic;
-[ap, apold, rec, prec, fp, tp, is_correct] = get_aps(params,cls,gt,npos,BB);
+[ap, rec, prec, fp, tp, is_correct, missed] = get_aps(params,cls,gt,npos,BB);
+
+result.rec = rec;
+result.prec = prec;
+result.ap = ap;
+result.fp = fp;
+result.tp=tp;
+result.npos=npos;
+result.is_correct = is_correct;
+result.missed = missed;
 
 finaltime = toc(sss);
 fprintf(1,'Time for computing AP: %.3fsec\n',finaltime);
 
-function [ap,apold,rec,prec,fp,tp,is_correct] = get_aps(params,cls,gt,npos,BB);
+function [ap,rec,prec,fp,tp,is_correct,missed] = get_aps(params,cls,gt,npos,BB);
 
 [~,order] = sort(BB(:,end),'descend');
 BB = BB(order,:);
-%confidence = con
-% sort detections by decreasing confidence
-%[sc,si]=sort(-confidence);
-%ids=ids(si);
-%BB=BB(:,si);
-
-
 confidence = BB(:,end);
 
 % assign detections to ground truth objects
@@ -121,33 +62,13 @@ fp=zeros(nd,1);
 
 tic;
 
-%do instead of slow hash
-%[~,iii] = ismember(ids,gtids);
-
 is_correct = zeros(nd,1);
 for d=1:nd
-    % display progress
-    % if toc>1
-    %     fprintf('%s: pr: compute: %d/%d\n',cls,d,nd);
-    %     drawnow;
-    %     tic;
-    % end
-    
-    % find ground truth image
-    %i=VOChash_lookup(hash,ids{d});
-    % i = iii(d);
-    % if isempty(i)
-    %     error('unrecognized image "%s"',ids{d});
-    % elseif length(i)>1
-    %     error('multiple image "%s"',ids{d});
-    % end
 
     % assign detection to ground truth object if any
     bb = BB(d,1:4);
     i = BB(d,11);
-    %bb=BB(:,d);
     ovmax=-inf;
-
 
     for j=1:size(gt(i).BB,2)
         bbgt=gt(i).BB(:,j);
@@ -190,17 +111,16 @@ tp=cumsum(tp);
 rec=tp/npos;
 prec=tp./(fp+tp);
 
-% compute average precision
-ap=0;
-for t=0:0.1:1
-    p=max(prec(rec>=t));
-    if isempty(p)
-        p=0;
-    end
-    ap=ap+p/11;
-end
-
-apold = ap;
+% compute average precision the old way
+% ap=0;
+% for t=0:0.1:1
+%     p=max(prec(rec>=t));
+%     if isempty(p)
+%         p=0;
+%     end
+%     ap=ap+p/11;
+% end
+% apold = ap;
 ap = VOCap(rec,prec);
 
 if params.display
@@ -214,7 +134,25 @@ if params.display
     xlabel('recall','FontSize',20)
     ylabel('precision','FontSize',20)
     title(sprintf('class: %s, AP = %.3f, OS=%.3f',...
-                  cls, ap, params.evaluation_minoverlap),'FontSize',20);
+                  cls, ap, params.evaluation_minoverlap),...
+          'FontSize',20);
     maxrec = max(rec)+.1;
     axis([0 1 0 1])
 end
+
+dets = cat(1,gt.det);
+objs = cat(2,gt.objectid);
+ims = cat(2,gt.imageid);
+missed.dets = dets;
+missed.objs = objs;
+missed.ims = ims;
+bads = find(missed.dets==0);
+allbb=cat(2,gt.BB)';
+missed.bbs = allbb(bads,:);
+missed.dets = missed.dets(bads);
+missed.objs = missed.objs(bads);
+missed.ims = missed.ims(bads);
+
+
+%save resdata.mat ims objs dets 
+%keyboard
