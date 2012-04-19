5cbf18412c03ae39e2368ad0f76ca07345b537b4
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-04-19 11:14:37 -0400

    Adding cov computation code

---
+++ covs/estimate_covariance.m
@@ -0,0 +1,156 @@
+function res = estimate_covariance(train_set, cparams, res2)
+% Estimates the covariance matrix of all windows of size hg_size
+% inside PASCAL VOC set "train_set_name"
+% If res2 is provided (optional), the highest probability window is
+% diaplayed in each image
+
+%Estimates the covariance matrix by going through all the data
+
+if isfield(cparams,'titler')
+  
+  filer = sprintf('/nfs/baikal/tmalisie/cov/covar_%s_%d_%d.mat',...
+                  cparams.titler, cparams.hg_size(1), ...
+                  cparams.hg_size(2));
+  
+  if fileexists(filer)
+    fprintf(1,'FILE EXISTS, loading from %s\n',filer);
+    load(filer)
+    return;
+  end
+end
+
+addpath(genpath(pwd));
+
+if ~exist('data_directory','var')
+  data_directory = '/Users/tomasz/projects/pascal/';
+end
+
+if ~exist('dataset_directory','var')
+  dataset_directory = 'VOC2007';
+end
+
+
+%keep all windows
+params = esvm_get_default_params;
+params.detect_max_windows_per_exemplar = 1000000;
+params.detect_save_features = 1;
+params.detect_exemplar_nms_os_threshold = 1;
+params.detect_pyramid_padding = 0;
+params.detect_add_flip = 0;
+
+model.params = params;
+model.models{1}.w = zeros(cparams.hg_size(1),...
+                          cparams.hg_size(2),...
+                          params.init_params.features());
+model.models{1}.b = 0;
+model.models{1}.hg_size = cparams.hg_size;
+
+n = 0;
+globalN = 0;
+fsize = numel(model.models{1}.w);
+sums = zeros(fsize, 1);
+outers = zeros(fsize, fsize);
+r = randperm(length(train_set));
+r = r(1:min(length(r),1000));
+train_set = train_set(r);
+
+if exist('res2','var')
+  tic
+  % [u,w,v] = svd(res2.c);
+  % [aa,bb] = sort(diag(w),'descend');
+  % w(w<aa(10))=0;
+  % invcov = v'*1./(w'+eps)*u';  
+  %invcov = pinv(res2.c);
+  toc
+end
+
+for i = 1:length(train_set)
+  I = toI(train_set{i});
+  rs = esvm_detect(I,model);
+  if numel(rs.xs{1})==0
+    fprintf(1,'tiny image!\n');
+    continue
+  end
+
+  x = cat(2,rs.xs{1}{:});
+  bbs = rs.bbs{1};
+  globalN = globalN + size(x,2);
+  
+  if isfield(cparams,'obj') && length(cparams.obj) > 0
+    
+    if length(train_set{i}.objects) == 0
+      continue
+    end
+    
+    curcls = find(ismember({train_set{i}.objects.class}, ...
+                           cparams.obj));
+    if strcmp(cparams.obj,'all')
+      curcls = 1:length(train_set{i}.objects);
+    end
+    
+    if length(curcls) == 0
+      continue
+    end
+    
+    goodbb = cat(1,train_set{i}.objects.bbox);
+    goodbb = goodbb(curcls,:);
+    
+    if (cparams.obj_os < 0)
+      goods = find(max(getosmatrix_bb(bbs,goodbb),[],2) <= ...
+                   -cparams.obj_os);
+    else
+    
+      goods = find(max(getosmatrix_bb(bbs,goodbb),[],2) >= ...
+                   cparams.obj_os);
+    end
+    x = x(:,goods);
+    bbs = bbs(goods,:);
+    % figure(1)
+    % clf
+    % imagesc(I)
+    % plot_bbox(bbs)
+    % drawnow
+  elseif isfield(cparams,'scene_os')
+    scene_bb = [1 1 size(I,2) size(I,1)];
+    goods = find(getosmatrix_bb(bbs,scene_bb)>cparams.scene_os);
+    x = x(:,goods);
+    bbs = bbs(goods,:);
+  end
+
+  if exist('res2','var')
+    r = res2.w(:)'*x;
+    % x2 = -bsxfun(@minus,res2.mean,x);
+    % tic;r=diag(x2'*invcov*x2);toc
+    
+    [aa,bb] = sort(r,'descend');
+    for q = 1:1 %length(bb)
+      figure(33)
+      clf
+      imagesc(I)
+      plot_bbox(bbs(bb(q),:))
+      title(sprintf('rank %d/%d scoree=%.3f',q,length(bb),aa(q)));
+      drawnow
+    end
+  end
+  sums = sums + sum(x,2);
+  curn = size(x,2);
+  
+  outers = outers + x*x';
+  n = n + curn;
+  fprintf(1,sprintf('i=%05d/%05d n = %d/%d\n',i,length(train_set), ...
+                    n,globalN));
+end
+
+
+res.c = 1./(n-1)*(outers - sums*sums'/n);
+res.n = n;
+res.mean = sums/n;
+res.params = params;
+res.hg_size = size(model.models{1}.w);
+[res.evecicons,res.evals] = show_cov(res);
+res.titler = '';
+
+if exist('filer','var')
+  res.titler = cparams.titler;
+  save(filer,'res');
+end
---
+++ covs/script_compute_covars.m
@@ -0,0 +1,22 @@
+%Script which computes covariances for a few different datasets
+%sets = 'trainval'
+if ~exist('data_set','var')
+  load ~/projects/pascal/VOC2007/trainval.mat 
+end
+cls = {'bus','car','train','cow','sheep','chair','sofa'};
+cls = {'all'};
+
+for a = 8:12
+  for b = 8:12
+    params.hg_size = [a b];
+    for i = 1:length(cls)  
+  %[cur_pos_set, cur_neg_set] = get_positive_negative_sets(data_set, ...
+  %                                                cls{i});
+  fprintf(1,'Set is %s, hg_size = [%d,%d],\n',...
+          cls{i}, params.hg_size(1), params.hg_size(2));
+  params.titler = ['all'];
+  [res] = estimate_covariance(data_set, params);
+    
+  end
+  end
+end
