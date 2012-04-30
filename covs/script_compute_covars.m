2bffa6aa094e6ee722dfc2f58f8e92f5ab3fc445
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-04-30 03:14:18 -0400

    use split

--- covs/script_compute_covars.m
+++ covs/script_compute_covars.m
@@ -10,13 +10,13 @@ for a = 8:12
   for b = 8:12
     params.hg_size = [a b];
     for i = 1:length(cls)  
-  %[cur_pos_set, cur_neg_set] = get_positive_negative_sets(data_set, ...
-  %                                                cls{i});
-  fprintf(1,'Set is %s, hg_size = [%d,%d],\n',...
-          cls{i}, params.hg_size(1), params.hg_size(2));
-  params.titler = ['all'];
-  [res] = estimate_covariance(data_set, params);
-    
-  end
+      %[cur_pos_set, cur_neg_set] = split_sets(data_set, ...
+      %                                                cls{i});
+      fprintf(1,'Set is %s, hg_size = [%d,%d],\n',...
+              cls{i}, params.hg_size(1), params.hg_size(2));
+      params.titler = ['all'];
+      [res] = estimate_covariance(data_set, params);
+      
+    end
   end
 end
