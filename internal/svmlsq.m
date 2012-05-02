b82a4f1426fd071f466c7e198fe667c3dada6832
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-05-02 12:57:10 -0400

    perform svm learning

--- internal/svmlsq.m
+++ internal/svmlsq.m
@@ -12,6 +12,29 @@ function [w,svmobj] = svmlsq(y,x,lambda,w)
 %  w: current estimate of w
 %Tomasz Malisiewicz (tomasz@csail.mit.edu)
 
+% [w,svmobj] = learn_ll(x,y,lambda,1);
+
+% function [w,svmobj,t] = learn_ll(x,y,lambda,meancoeff)
+% starttime = tic;
+
+% if ~exist('meancoeff','var')
+%   meancoeff = 0;
+% end
+
+% params.bias = 100;
+% mx = mean(x,2)*meancoeff;
+% x2 = bsxfun(@minus,x,mx);
+% svm_model = liblineartrain(y, sparse(x2)',sprintf(['-s 2 -B %.5f -e .0001 -c' ...
+%                     ' %f -q'], params.bias, 1/lambda));
+% w = svm_model.w';
+% w(end) = (w(end)*params.bias);
+% t = toc(starttime);
+
+% w(end) = w(end) - w(1:end-1)'*mx;
+
+% vals = y'.*(w(1:end-1)'*x+w(end));
+% svmobj =  lambda/2*sum(w(1:end-1).^2) + sum(hinge(vals));
+% return;
 
 %maximum number of newton iterations
 NITER = 20;
@@ -55,6 +78,8 @@ for i = 1:NITER
     goods = find(r<=1.0);
   end
   
+
+  %curmat = x(:,goods)*x(:,goods)';
   newgoods = setdiff(goods,oldgoods);
   oldgoods = setdiff(oldgoods,goods);
   curmat = curmat + x(:,newgoods)*x(:,newgoods)' - x(:,oldgoods)*x(:,oldgoods)';
@@ -77,18 +102,22 @@ for i = 1:NITER
   
 
   %perform the line search  
-  [w,bestobj] = line_search(w,oldw,y,x,lambda,linspace(0,1,10));
-    
+  bestobj = lambda/2*sum(w(1:end-1).^2) + sum(hinge(y'.*(w(1:end- ...
+                                                  1)'*x+w(end))));
 
-  if (oldobj - bestobj)/oldobj < .0001
-    break;
-  end
-  oldw = w;  
+  [w2,bestobj] = line_search(w,oldw,y,x,lambda,linspace(0,1,10));
+
   
   %svmobj =  lambda/2*sum(w(1:end-1).^2) + sum(hinge(y'.*(w'*x)));
   endtime = toc(starttime);
   fprintf(1,' ---curobj=%.3f (iter in %.3f s, +s: %d -s: %d)\n',...
           bestobj,endtime,length(newgoods),length(oldgoods));
+  
+  if (oldobj - bestobj)/oldobj < .001
+    break;
+  end
+  oldw = w;  
+
 
   oldobj = bestobj;
   oldgoods = goods;
@@ -125,14 +154,16 @@ end
 bestw = w;
 bestobj = 100000000;
 r1 = y'.*(w(1:end-1)'*x+w(end));
-r2 = y'.*(oldw(1:end-1)'*x+w(end));
+r2 = y'.*(oldw(1:end-1)'*x+oldw(end));
 n1 = sum(w(1:end-1).^2);
 n2 = sum(oldw(1:end-1).^2);
 ip = w(1:end-1)'*oldw(1:end-1);
 for q = 1:length(alphas)
   alpha = alphas(q);
   
-  newobj(q) = lambda/2*alpha*alpha*n1+(1-alpha).^2*n2+2*alpha*(1-alpha)*ip ...
+  newobj(q) = lambda/2*alpha^2*n1+...
+      (1-alpha).^2*n2+...
+      2*alpha*(1-alpha)*ip ...
       + sum(hinge(r1*alpha+(1-alpha)*r2));
   
   if (newobj(q) < bestobj)
