d0069b82c83f1c0ddfa880417a2bc1a8f5b85758
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-05-11 15:45:41 -0400

    keep track of conversion process

--- internal/unify_model.m
+++ internal/unify_model.m
@@ -2,13 +2,13 @@ function model2 = unify_model(model)
 %Convert separate "initialized" Exemplar-SVM models into one big
 %fat model which will start a DT-training run
 
-
 for i = 1:length(model.models)
   bbs{i} = model.models{i}.bb;
   bbs{i}(:,6) = i+(length(model.models))*bbs{i}(:,7);
   gts{i} = [model.models{i}.gt_box 0 i 0 0 0 0 model.models{i}.curid ...
             0];
   curx{i} = model.models{i}.x(:,1);
+  curx{i}(5) = i;
   curbb{i} = bbs{i}(1,:);
 end
 
@@ -40,12 +40,18 @@ model2.models{1}.savebb = bb;
 
 model2.models{1}.w = reshape(mean(model2.models{1}.x,2), ...
                              size(model2.models{1}.w));
-model2.models{1}.w = model2.models{1}.w - mean(model2.models{1}.w(:));
+model2.models{1}.w = model2.models{1}.w - mean(model2.models{1}.w(: ...
+                                                  ));
+
+model2.models{1}.b = -100;
+model2.models{1}.savebb(:,5) = 1:size(model2.models{1}.savebb,1);
 
 %model2 = esvm_update_positives(model2,1);
 
 hg_size = model.params.init_params.hg_size;
-model.models{1}.center = [0 0 10*hg_size(1) 10*hg_size(2)];
+model2.models{1}.center = [0 0 10*hg_size(1) 10*hg_size(2)];
 
+[aa,bb] = ismember(model2.models{1}.bb(:,[1:4 7 11]), ...
+                   model2.models{1}.savebb(:,[1:4 7 11]),'rows');
 
-
+model2.models{1}.bb(:,5) = bb;
