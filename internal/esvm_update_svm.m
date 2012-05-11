30730fb397bf13810a120883d402db68dc559b07
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-05-11 15:37:08 -0400

    function needs cleanup

--- internal/esvm_update_svm.m
+++ internal/esvm_update_svm.m
@@ -1,4 +1,4 @@
-function [m, other] = esvm_update_svm(m,fraction)
+function [m, other] = esvm_update_svm(m,fraction,min_cache)
 % Perform SVM learning for a single exemplar model, we assume that
 % the exemplar has a set of detections loaded in m.svxs and m.svbbs
 % Durning Learning, we can apply some pre-processing such as PCA or
@@ -37,40 +37,30 @@ if isfield(m,'mask') && ~islogical(m.mask)
 end
 
 
-%NOTE: MAXSIZE is the maximum number of examples we will keep in our cache
-MAXSIZE = m.params.train_max_negatives_in_cache;
-if size(m.svxs,2) > MAXSIZE
-  fprintf(1,'WARNING:reducing SVM problem problem from %d to %d\n',...
-          size(m.svxs,2),MAXSIZE);
+% %NOTE: MAXSIZE is the maximum number of examples we will keep in our cache
+% MAXSIZE = m.params.train_max_negatives_in_cache;
+% if size(m.svxs,2) > MAXSIZE
+%   fprintf(1,'WARNING:reducing SVM problem problem from %d to %d\n',...
+%           size(m.svxs,2),MAXSIZE);
   
-  r = m.w(:)'*m.svxs;
-  [tmp,r] = sort(r,'descend');
-  r = r(1:MAXSIZE);
+%   r = m.w(:)'*m.svxs;
+%   [tmp,r] = sort(r,'descend');
+%   r = r(1:MAXSIZE);
   
-  %I used to think random was better
-  %r = HALFSIZE+randperm(length(r((HALFSIZE+1):end)));
-  %r = r(1:HALFSIZE);
-  %r = [r1 r];
-  m.svxs = m.svxs(:,r);
-  m.svbbs = m.svbbs(r,:);
-end
+%   %I used to think random was better
+%   %r = HALFSIZE+randperm(length(r((HALFSIZE+1):end)));
+%   %r = r(1:HALFSIZE);
+%   %r = [r1 r];
+%   m.svxs = m.svxs(:,r);
+%   m.svbbs = m.svbbs(r,:);
+% end
 
 %% here we take the best positive from the set of possible positives
 
-if exist('fraction','var') 
+if exist('fraction','var') && size(m.svxs,2) >= min_cache
   %take a random fraction of positives
   r = randperm(size(m.x,2));
   r = r(1:round(length(r)*fraction));
-  % [aa,bb] = sort(m.w(:)'*m.x - m.b);
-  % maxneg = max(m.w(:)'*m.svxs - m.b);
-  
-  % r = [m.w(:)'*m.x-m.b m.w(:)'*m.svxs-m.b];
-  % %y = [ones(1,size(m.x,2)) -1*ones(1,size(m.svxs,2))];
-  
-  % bb = bb(aa>maxneg);
-  % aa = aa(aa>maxneg);
-  % r = bb;
-
 else
   r = 1:size(m.x,2);
 end
@@ -78,7 +68,7 @@ end
 fprintf(1,'Fraction of positives is %d/%d\n',length(r),size(m.x, ...
                                                   2));
   
-superx = cat(2,m.x(:,r),m.svxs);
+superx = cat(2,m.x(logical(m.mask),r),m.svxs(logical(m.mask),:));
 supery = cat(1,ones(length(r),1),-1*ones(size(m.svxs,2),1));
 
 spos = sum(supery==1);
@@ -95,8 +85,8 @@ wneg = 1;
 %   wneg = wneg / wneg;
 % end
 
-A = eye(size(superx,1));
-mu = zeros(size(superx,1),1);
+%A = eye(size(superx,1));
+%mu = zeros(size(superx,1),1);
 
 % if mining_params.DOMINANT_GRADIENT_PROJECTION == 1  
 %   A = get_dominant_basis(reshape(mean(m.x(:,1),2), ...
@@ -124,14 +114,14 @@ mu = zeros(size(superx,1),1);
 %   A(:,end) = A(:,end) / norm(A(:,end));
 % end
 
-newx = bsxfun(@minus,superx,mu);
-newx = newx(logical(m.mask),:);
+%newx = bsxfun(@minus,superx(logical(m.mask),:),mu(logical(m.mask)));
+
 %newx = A(m.mask,:)'*newx;
 %keyboard
 %newx = newx(find(m.mask),:);
 
 fprintf(1,' -----\nStarting SVM: dim=%d... #pos=%d, #neg=%d ',...
-        size(newx,1),spos,sneg);
+        size(superx,1),spos,sneg);
 starttime = tic;
 
 
@@ -142,8 +132,8 @@ fprintf(1,'starting svmlsq:\n');
 oldw = [m.w(:)];
 oldw(end+1) = -m.b;
 
-[nw] = svmlsq(supery,newx, ...
-              (1/m.params.train_svm_c),oldw);
+[nw] = svmlsq(supery,superx, ...
+              (1/m.params.train_svm_c),oldw,m.params.train_newton_iter);
 
 svm_model.w = nw';
 wex = nw(1:end-1);
@@ -151,7 +141,7 @@ b = nw(end)*-1;
 
 learning_time = toc(starttime);
 
-vals = supery'.*(wex'*newx-b);
+vals = supery'.*(wex'*superx-b);
 svmobj =  (1/m.params.train_svm_c)/2*sum(wex.^2) + sum(hinge(vals));
 
 
@@ -165,8 +155,8 @@ if sneg == 0
 else
     
   %% project back to original space
-  b = b + wex'*A(m.mask,:)'*mu(m.mask);
-  wex = A(m.mask,:)*wex;
+  %b = b + wex'*A(m.mask,:)'*mu(m.mask);
+  %wex = A(m.mask,:)*wex;
   
   wex2 = zeros(size(superx,1),1);
   wex2(m.mask) = wex;
@@ -179,9 +169,11 @@ else
   end  
 end
 
+rpos = wex(:)'*m.x - b;
 maxpos = max(wex(:)'*m.x - b);
-minpos = min(wex(:)'*m.x - b);
-maxneg = max(wex(:)'*m.svxs - b);
+minpos = min(rpos);
+rneg = m.w(:)'*m.svxs - m.b;
+maxneg = max(rneg);
 
 fprintf(1,'\n --- Positives (Max,min) = %.3f,%.3f \n --- Negatives (Max) = %.3f\n',...
         maxpos,minpos,maxneg);
@@ -190,10 +182,9 @@ fprintf(1,'SVM iteration took %.3f sec, ',learning_time);
 m.w = reshape(wex, size(m.w));
 m.b = b;
 
-r = m.w(:)'*m.svxs - m.b;
-svs = find(r >= -1.0000);
+svs = find(rneg >= -1.0000);
 
-fprintf(1,'Length of nsvs is %d/%d\n',length(svs),length(r));
+fprintf(1,'Length of nsvs is %d/%d\n',length(svs),length(rneg));
 
 % if length(svs) == 0
 %   error('Something went wrong');
@@ -205,14 +196,14 @@ fprintf(1,'Length of nsvs is %d/%d\n',length(svs),length(r));
 %                    length(svs));
 
 %keep cache-full worth of negatives
-total_length = length(r);
-total_length = min(total_length,m.params.train_max_negatives_in_cache);
+total_length = min(length(rneg),...
+                   m.params.train_max_negatives_in_cache);
 
-[alpha,beta] = sort(r,'descend');
-svs = beta(1:min(length(beta),total_length));
+[alpha,beta] = sort(rneg,'descend');
+svs = beta(1:total_length);
 m.svxs = m.svxs(:,svs);
 m.svbbs = m.svbbs(svs,:);
-m.svbbs(:,end) = m.w(:)'*m.svxs-m.b;
-m.bb(:,end) = m.w(:)'*m.x-m.b;
+m.svbbs(:,end) = rneg(svs);
+m.bb(:,end) = rpos;
 %fprintf(1,' kept %d negatives\n',total_length);
 
