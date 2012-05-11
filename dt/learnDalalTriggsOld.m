7017cc8f9c7ad980b162288b296f44135ce71264
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-05-11 16:03:47 -0400

    rename files around

---
+++ dt/learnDalalTriggsOld.m
@@ -0,0 +1,87 @@
+function model = learnDalalTriggs(data_set, cls, params)
+% Learn a DalalTriggs template detector, with latent updates,
+% perturbed assignments (which help avoid local minima), and
+% ability to use different features.
+%
+% Copyright (C) 2011-12 by Tomasz Malisiewicz
+% All rights reserved. 
+%
+% This file is part of the Exemplar-SVM library and is made
+% available under the terms of the MIT license (see COPYING file).
+% Project homepage: https://github.com/quantombone/exemplarsvm
+
+addpath(genpath(pwd));
+
+if ~exist('cls','var')
+  error('Needs class as input');
+end
+
+if length(data_set) == 0
+  fprintf(1,'No dataset provided, loading default DATA');
+  
+  data_directory = '/Users/tomasz/projects/pascal/';
+  dataset_directory = 'VOC2007';  
+  data_set = install_dataset(data_directory, dataset_directory);
+end
+
+if ~exist('params','var') || length(params) == 0
+  %% Get default parameters
+  params = esvm_get_default_params;
+  params.display = 0;
+  params.dump_images = 0;
+  params.detect_max_windows_per_exemplar = 5000;
+  params.train_max_negatives_in_cache = 20000;
+  params.max_number_of_positives = 2000;
+  params.train_max_mined_images = 10000; %500;
+  params.latent_iterations = 2;
+  params.train_svm_c = .01;
+  params.train_max_windows_per_iteration = 3000;
+  % for dalaltriggs, it seams having same constant on positives as
+  % negatives is better than using 50
+  params.train_positives_constant = 1;
+  params.mine_from_negatives = 0;
+  params.mine_from_positives = 1;
+  params.mine_skip_positive_objects_os = .2;
+  params.mine_from_positives_do_latent_update = 1;
+  params.train_max_scale = 1.0;
+  params.latent_os_thresh = 0.5;
+  params.dt_initialize_with_flips = 0;
+  %params.detect_pyramid_padding = 0;
+  %fprintf(1,'hack max 40\n');
+  %params.max_number_of_positives = 20;
+else
+  params = esvm_get_default_params(params);
+end
+
+starttime = tic;
+model = esvm_initialize_dt(data_set, cls, params);
+
+%params.mine_from_positives = 1;
+%params.mine_from_negatives = 1;
+%params.train_max_mined_images = 3000;
+
+%model.params = esvm_get_default_params(params);
+model = esvm_train(model);
+
+% params.train_max_mined_images = 1000;
+% params.mine_from_negatives = 1;
+% params.mine_from_positives = 0;
+% model.params = esvm_get_default_params(params);
+% model = esvm_train(model);
+
+if 0
+for niter = 1:params.latent_iterations
+  %model = esvm_latent_update_dt(model);
+  params.mine_from_positives = 1;
+  params.mine_from_negatives = 0;
+  model.params = esvm_get_default_params(params);
+  model = esvm_train(model);
+  
+  params.mine_from_positives = 0;
+  params.mine_from_negatives = 1;
+  model.params = esvm_get_default_params(params);
+  model = esvm_train(model);
+end
+end
+
+model.learn_time = toc(starttime);
--- learnDalalTriggs.m
+++ learnDalalTriggs.m
@@ -1,87 +1,91 @@
-function model = learnDalalTriggs(data_set, cls, params)
-% Learn a DalalTriggs template detector, with latent updates,
-% perturbed assignments (which help avoid local minima), and
-% ability to use different features.
-%
-% Copyright (C) 2011-12 by Tomasz Malisiewicz
-% All rights reserved. 
-%
-% This file is part of the Exemplar-SVM library and is made
-% available under the terms of the MIT license (see COPYING file).
-% Project homepage: https://github.com/quantombone/exemplarsvm
 
-addpath(genpath(pwd));
-
-if ~exist('cls','var')
-  error('Needs class as input');
+%ok
+if ~exist('trainval','var')
+  trainval=load('~/projects/pascal/VOC2007/trainval.mat');
+  trainval = trainval.data_set;
 end
 
-if length(data_set) == 0
-  fprintf(1,'No dataset provided, loading default DATA');
-  
-  data_directory = '/Users/tomasz/projects/pascal/';
-  dataset_directory = 'VOC2007';  
-  data_set = install_dataset(data_directory, dataset_directory);
-end
+%classes = {'chair','tvmonitor','cow','motorbike','bus','sofa'};
+classes = {'bicycle','tvmonitor','bottle','bus','chair','bus','cow','bottle','tvmonitor','bicycle','motorbike'};
+for i = 1:length(classes)
+  cls = classes{i};
 
-if ~exist('params','var') || length(params) == 0
-  %% Get default parameters
-  params = esvm_get_default_params;
-  params.display = 0;
-  params.dump_images = 0;
-  params.detect_max_windows_per_exemplar = 5000;
-  params.train_max_negatives_in_cache = 20000;
-  params.max_number_of_positives = 2000;
-  params.train_max_mined_images = 10000; %500;
-  params.latent_iterations = 2;
-  params.train_svm_c = .01;
-  params.train_max_windows_per_iteration = 3000;
-  % for dalaltriggs, it seams having same constant on positives as
-  % negatives is better than using 50
-  params.train_positives_constant = 1;
-  params.mine_from_negatives = 0;
-  params.mine_from_positives = 1;
-  params.mine_skip_positive_objects_os = .2;
-  params.mine_from_positives_do_latent_update = 1;
-  params.train_max_scale = 1.0;
-  params.latent_os_thresh = 0.5;
-  params.dt_initialize_with_flips = 0;
-  %params.detect_pyramid_padding = 0;
-  %fprintf(1,'hack max 40\n');
-  %params.max_number_of_positives = 20;
+%[positive_set, negative_set] = ...
+%    split_sets(trainval, cls);
+%data_set = cat(2,data_set,negative_set');
+data_set = trainval;
+
+params = esvm_get_default_params;
+params.display = 0;
+params.dump_images = 0;
+params.detect_max_windows_per_exemplar = 100;
+params.max_number_of_positives = 2000;
+
+params.train_max_negatives_in_cache = 30000;
+params.train_max_mined_images = 5000;
+params.train_max_negative_images = 10000;
+params.train_max_mine_iterations = 100;
+%params.train_svm_c = .0001;
+params.train_svm_c = .01;
+params.train_max_windows_per_iteration = 5000;
+params.train_positives_constant = 1;
+params.train_max_scale = 1.0;
+params.train_max_images_per_iteration = 200;
+params.detect_pyramid_padding = 2;
+params.detect_levels_per_octave = 10;
+
+params.mine_from_negatives = 1;
+params.mine_from_positives = 1;
+params.mine_skip_positive_objects_os = .1;
+params.mine_from_positives_do_latent_update = 0;
+
+params.latent_os_thresh = 0.5;
+params.init_params.MAX_POS_CACHE = 5000;
+params.init_params.ADD_LR = 1;
+params.init_params.init_function = ...
+    @esvm_initialize_fixedframe_exemplar;
+%params.training_function = @ ...
+%    (m)esvm_update_svm(esvm_update_positives(esvm_update_svm(m,.8)));
+
+params.training_function = @ ...
+    (m)esvm_update_positives(esvm_update_svm(esvm_update_positives(esvm_update_svm(m,.8))));
+
+%hardcode mask size
+%params.init_params.hg_size = [10 10 31];
+
+filer = sprintf('~/Desktop/inits/%s.mat',cls);
+if fileexists(filer)
+  fprintf(1,'loading %s\n',filer);
+  load(filer);
 else
-  params = esvm_get_default_params(params);
+  model = esvm_initialize_exemplars(data_set, cls, params);
+  model.params.display = 1;
+  model = unify_model(model);
+  save(filer,'model');
 end
 
-starttime = tic;
-model = esvm_initialize_dt(data_set, cls, params);
+params.display = 1;
+model.params = params;
 
-%params.mine_from_positives = 1;
-%params.mine_from_negatives = 1;
-%params.train_max_mined_images = 3000;
+model.models{1}.params = params;
+model.models{1}.params.train_svm_c = model.params.train_svm_c;
+model.models{1}.params.train_newton_iter = 10;
+model.params.train_newton_iter = 10;
 
-%model.params = esvm_get_default_params(params);
-model = esvm_train(model);
-
-% params.train_max_mined_images = 1000;
-% params.mine_from_negatives = 1;
-% params.mine_from_positives = 0;
-% model.params = esvm_get_default_params(params);
-% model = esvm_train(model);
-
-if 0
-for niter = 1:params.latent_iterations
-  %model = esvm_latent_update_dt(model);
-  params.mine_from_positives = 1;
-  params.mine_from_negatives = 0;
-  model.params = esvm_get_default_params(params);
+filer = sprintf('~/Desktop/DT/%s.mat',cls);
+if fileexists(filer)
+  load(filer);
+else
   model = esvm_train(model);
-  
-  params.mine_from_positives = 0;
-  params.mine_from_negatives = 1;
-  model.params = esvm_get_default_params(params);
-  model = esvm_train(model);
-end
+  save(filer,'model');
 end
 
-model.learn_time = toc(starttime);
+filer = sprintf('~/Desktop/ESVM/%s.mat',cls);
+if fileexists(filer)
+  load(filer)
+else
+  model = mixtures_real(model);
+  save(filer,'model');
+end
+
+end
