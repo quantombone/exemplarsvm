4401a2537100144c819b1ad0517ff06c67b970d3
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-05-28 06:51:42 -0400

    use default -1 threshold

--- applyModel.m
+++ applyModel.m
@@ -16,7 +16,7 @@ function boxes = applyModel(data_set, model, draw)
 test_params = model.params;
 test_params.detect_exemplar_nms_os_threshold = 1.0;
 test_params.detect_max_windows_per_exemplar = 200;
-test_params.detect_keep_threshold = -1.5;
+test_params.detect_keep_threshold = -1.0;
 test_params.calibrate_nms = 1.0;
 
 if length(data_set) == 0
